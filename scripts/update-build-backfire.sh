92251126269f416f12f5c18c3bc28593eaf933d4
Patrick Grimm <patrick@lunatiki.de>
2011-06-19 23:20:28 +0200

    [scripst]

--- scripts/update-build-backfire.sh
+++ scripts/update-build-backfire.sh
@@ -10,74 +10,36 @@ timestamp=`date "+%F_%H-%M"`
 echo $timestamp >timestamp
 date +"%Y/%m/%d %H:%M">VERSION.txt
 
-#svn co https://www.wgaugsburg.de/svn/kamikaze
-#if [ -d wgaugsburg ] ; then
-#	echo "update wgaugsburg svn up"
-#	cd wgaugsburg
-#	rm -rf $(svn status)
-#	if [ -z $wgaugsburg_revision ] ; then
-#		svn up
-#	else
-#		svn sw -r $packages_revision https://www.wgaugsburg.de/svn/kamikaze
-#	fi
-#	cd ../
-#else
-#	echo "create wgaugsburg svn co"
-#	svn co https://www.wgaugsburg.de/svn/kamikaze wgaugsburg
-#	if [ -z $wgaugsburg_revision ] ; then
-#		svn co https://www.wgaugsburg.de/svn/kamikaze wgaugsburg
-#	else
-#		svn co https://www.wgaugsburg.de/svn/kamikaze wgaugsburg
-#		cd wgaugsburg
-#		svn sw -r $packages_revision https://www.wgaugsburg.de/svn/kamikaze
-#		cd ..
-#	fi
-#fi
-
-#svn http://wurststulle.dyndns.org/yaffmap/svn/trunk/update_agents/package/yaffmap_uci/
-if [ -d update_agents ] ; then
-	echo "update yaffmap_uci svn up"
-	cd update_agents
-	rm -rf $(svn status)
-	if [ -z $update_agents_revision ] ; then
-		svn up
-	else
-		svn sw -r $update_agents_revision http://wurststulle.dyndns.org/yaffmap/svn/trunk/update_agents
-	fi
+if [ -d yaffmap-agent ] ; then
+	echo "update yaffmap-agent git pull"
+	cd yaffmap-agent
+	git pull || exit 0
 	cd ../
 else
-	echo "create yaffmap_uci svn co"
-	#svn co http://wurststulle.dyndns.org/yaffmap/svn/trunk/ update_agents
-	if [ -z $update_agents_revision ] ; then
-		svn co http://wurststulle.dyndns.org/yaffmap/svn/trunk/update_agents update_agents
-	else
-		svn co http://wurststulle.dyndns.org/yaffmap/svn/trunk/update_agents update_agents
-		cd update_agents
-		svn sw -r $update_agents_revision http://wurststulle.dyndns.org/yaffmap/svn/trunk/update_agents
-		cd ..
-	fi
+	echo "create yaffmap-agent git clone"
+	#git clone git://github.com/wurststulle/yaffmap-agent.git || exit 0
+	git clone git://github.com/stargieg/yaffmap-agent.git || exit 0
 fi
-
 
 if [ -d packages ] ; then
 	echo "update packages svn up"
 	cd packages
 	rm -rf $(svn status)
 	if [ -z $packages_revision ] ; then
-		svn up
+		svn up  || exit 0
 	else
-		svn sw -r $packages_revision svn://svn.openwrt.org/openwrt/packages
+		svn sw -r $packages_revision svn://svn.openwrt.org/openwrt/packages || exit 0
 	fi
 	cd ../
 else
 	echo "create packages svn co"
 	svn co svn://svn.openwrt.org/openwrt/packages packages
 	if [ -z $packages_revision ] ; then
-		svn co svn://svn.openwrt.org/openwrt/packages packages
+		svn co svn://svn.openwrt.org/openwrt/packages packages || exit 0
 	else
-		svn co svn://svn.openwrt.org/openwrt/packages packages
+		svn co svn://svn.openwrt.org/openwrt/packages packages || exit 0
 		cd packages
-		svn sw -r $packages_revision svn://svn.openwrt.org/openwrt/packages
+		svn sw -r $packages_revision svn://svn.openwrt.org/openwrt/packages || exit 0
 		cd ..
 	fi
 fi
@@ -89,34 +51,40 @@ PACKAGESPATCHES="$PACKAGESPATCHES radvd-ifconfig.patch"
 PACKAGESPATCHES="$PACKAGESPATCHES olsrd.init_6and4-patches.patch"
 PACKAGESPATCHES="$PACKAGESPATCHES package-collectd.patch"
 PACKAGESPATCHES="$PACKAGESPATCHES olsrd.config-rm-wlan-patches.patch"
+PACKAGESRPATCHES="$PACKAGESRPATCHES packages-r27157.patch"
 
 for i in $PACKAGESPATCHES ; do
 	pparm='-p0'
-	echo "Patch: $i"
+	#echo "Patch: $i"
+	patch $pparm < ../ff-control/patches/$i || exit 0
+done
+for i in $PACKAGESRPATCHES ; do
+	pparm='-p0 -R'
+	#echo "Patch: $i"
 	patch $pparm < ../ff-control/patches/$i || exit 0
 done
 rm -rf libs/mysql
 cd ..
 
 #update and patch repos
-#if [ -d packages-pberg ] ; then
-#	echo "update packages-pberg git pull"
-#	cd packages-pberg
-#	git pull
-#	cd ../
-#else
-#	echo "create packages-pberg git clone"
-#	git clone git://github.com/stargieg/packages-pberg.git
-#fi
+if [ -d packages-pberg ] ; then
+	echo "update packages-pberg git pull"
+	cd packages-pberg
+	git pull || exit 0
+	cd ../
+else
+	echo "create packages-pberg git clone"
+	git clone git://github.com/stargieg/packages-pberg.git || exit 0
+fi
 
 if [ -d piratenfreifunk-packages ] ; then
 	echo "update piratenfreifunk-packages manual git pull"
 	cd piratenfreifunk-packages
-	git pull
+	git pull || exit 0
 	cd ../
 else
 	echo "create piratenfreifunk-packages git clone"
-	git clone git://github.com/basicinside/piratenfreifunk-packages.git
+	git clone git://github.com/basicinside/piratenfreifunk-packages.git || exit 0
 fi
 
 if [ -d luci-master ] ; then
@@ -124,16 +92,17 @@ if [ -d luci-master ] ; then
 	cd luci-master
 	git checkout HEAD .
 	git add .
-	rm $(git diff --cached | grep 'diff --git a' | cut -d ' ' -f 3 | cut -b 3-)
+	rmf="$(git diff --cached | grep 'diff --git a' | cut -d ' ' -f 3 | cut -b 3-)"
+	[ -z "$rm" ] || rm "$rmf"
 	git reset --hard
-	git pull
-	[ -z $luci_revision ] || git checkout $luci_revision
+	git pull git://nbd.name/luci.git || exit 0
+	[ -z $luci_revision ] || git checkout $luci_revision || exit 0
 	cd ../
 else
 	echo "create HEAD master"
-	git clone git://nbd.name/luci.git luci-master
+	git clone git://nbd.name/luci.git luci-master || exit 0
 	cd luci-master
-	git checkout origin/master
+	git checkout origin/master || exit 0
 	cd ../
 fi
 
@@ -146,6 +115,7 @@ LUCIPATCHES="$LUCIPATCHES freifunk-muenster.patch"
 LUCIPATCHES="$LUCIPATCHES freifunk-cottbus.patch"
 LUCIPATCHES="$LUCIPATCHES freifunk-bno.patch"
 LUCIPATCHES="$LUCIPATCHES freifunk-pirate-ndb.patch"
+LUCIPATCHES="$LUCIPATCHES freifunk-pirate-ffwtal.patch"
 LUCIPATCHES="$LUCIPATCHES luci-freifunk_berlin.patch"
 LUCIPATCHES="$LUCIPATCHES luci-app-olsr-use-admin-mini.patch"
 LUCIPATCHES="$LUCIPATCHES luci-modfreifunk-use-admin-mini.patch"
@@ -159,9 +129,10 @@ LUCIPATCHES="$LUCIPATCHES luci-sys-routes6.patch"
 LUCIPATCHES="$LUCIPATCHES luci-app-statistics-add-madwifi-olsr.patch"
 LUCIPATCHES="$LUCIPATCHES luci-freifunk_radvd.patch"
 LUCIPATCHES="$LUCIPATCHES luci-freifunk_l2gvpn.patch"
+LUCIPATCHES="$LUCIPATCHES luci-app-splash-css.patch"
 for i in $LUCIPATCHES ; do
 	pparm='-p1'
-	echo "Patch: $i"
+	#echo "Patch: $i"
 	patch $pparm < ../ff-control/patches/$i || exit 0
 done
 
@@ -184,8 +155,8 @@ for board in $boards ; do
 	cd $verm/$board
 	echo "clean up"
 	rm -f .config
-#	rm -rf ./tmp
-##	rm -rf ./feeds/*
+	rm -rf ./tmp
+#	rm -rf ./feeds/*
 #	rm -rf ./feeds/*.tmp
 #	rm -rf ./feeds/*.index
 ##	rm -rf ./package/feeds/*
@@ -201,11 +172,25 @@ for board in $boards ; do
 	rm -rf ./files
 	mkdir -p ./files
 	rm -rf $(svn status)
-	svn co svn://svn.openwrt.org/openwrt/branches/$verm ./
+	case $verm in
+		trunk) 
+			svn co svn://svn.openwrt.org/openwrt/trunk ./  || exit 0
+			;;
+		*)
+			svn co svn://svn.openwrt.org/openwrt/branches/$verm ./ || exit 0
+			;;
+	esac
 	if [ -z $openwrt_revision ] ; then
-		svn up
+		svn up || exit 0
 	else
-		svn sw -r $openwrt_revision svn://svn.openwrt.org/openwrt/branches/$verm
+		case $verm in
+			trunk) 
+				svn sw -r $openwrt_revision svn://svn.openwrt.org/openwrt/trunk || exit 0
+				;;
+			*)
+				svn sw -r $openwrt_revision svn://svn.openwrt.org/openwrt/branches/$verm || exit 0
+				;;
+		esac
 	fi
 	openwrt_revision=$(svn info | grep Revision | cut -d ' ' -f 2)
 	cp ../../VERSION.txt VERSION.txt
@@ -223,18 +208,18 @@ for board in $boards ; do
 	echo "src-link piratenluci ../../../piratenfreifunk-packages" >> feeds.conf
 	echo "src-link luci ../../../luci-master" >> feeds.conf
 #	echo "src-link wgaugsburg ../../../wgaugsburg/packages" >> feeds.conf
-	echo "src-link update_agents ../../../update_agents" >> feeds.conf
+	echo "src-link yaffmapagent ../../../yaffmap-agent" >> feeds.conf
 	echo "openwrt feeds update"
 	scripts/feeds update
 	echo "openwrt feeds install"
 	scripts/feeds install -a
-	sed -i -e "s/downloads\.openwrt\.org.*/$servername\/$verm\/$ver-timestamp\/$timestamp\/$board\/packages/" package/opkg/files/opkg.conf
+	sed -i -e "s,downloads\.openwrt\.org.*,$servername/$verm/$ver-timestamp/$timestamp/$board/packages," package/opkg/files/opkg.conf
 	PATCHES="$PATCHES base-passwd-admin.patch"
 	PATCHES="$PATCHES base-system.patch"
 	PATCHES="$PATCHES routerstation-bridge-wan-lan.patch"
 	PATCHES="$PATCHES routerstation-pro-bridge-wan-lan.patch"
 	PATCHES="$PATCHES brcm-2.4-reboot-fix.patch"
-	PATCHES="$PATCHES ar5312_flash_4MB_flash.patch"
+#	PATCHES="$PATCHES ar5312_flash_4MB_flash.patch"
 	PATCHES="$PATCHES base-disable-ipv6-autoconf.patch"
 	PATCHES="$PATCHES package-crda-regulatory-pberg.patch"
 	#PATCHES="$PATCHES make-art-writeable.patch"
@@ -255,19 +240,20 @@ for board in $boards ; do
 #	svn co svn://svn.openwrt.org/openwrt/trunk/package/mac80211
 #	cd ../
 	cd package/mac80211
-	svn sw -r 26762 svn://svn.openwrt.org/openwrt/branches/backfire/package/mac80211
+#	svn sw -r 26762 svn://svn.openwrt.org/openwrt/branches/backfire/package/mac80211
+	svn sw -r 26686 svn://svn.openwrt.org/openwrt/branches/backfire/package/mac80211
 	cd ../../
 	mkdir -p ../../dl
 	[ -h dl ] || ln -s ../../dl dl
 	time nice -n 10 make V=99 world $make_options || ( rm update-build-$verm-$board.lock ; exit 1 )
 #	cp bin/$board/OpenWrt-ImageBuilder-$board-for-*.tar.bz2 ../
-	cp build_dir/target-$arch*/root-$board/usr/lib/opkg/status ../opkg-$board.status
+	cp build_dir/target-$arch*/root-*/usr/lib/opkg/status ../opkg-$board.status
 	mkdir -p 			$wwwdir/$verm/$ver-timestamp/$timestamp/$board
-	rsync -a --delete bin/$board/ 	$wwwdir/$verm/$ver-timestamp/$timestamp/$board
+	rsync -a --delete bin/*/ 	$wwwdir/$verm/$ver-timestamp/$timestamp/$board
 	cp VERSION.txt		 	$wwwdir/$verm/$ver-timestamp/$timestamp/$board
 	cp .config 			$wwwdir/$verm/$ver-timestamp/$timestamp/$board/dot-config
 	mkdir -p 			$wwwdir/$verm/$ver/$board
-	rsync -a --delete bin/$board/ 	$wwwdir/$verm/$ver/$board
+	rsync -a --delete bin/*/ 	$wwwdir/$verm/$ver/$board
 	cp VERSION.txt			$wwwdir/$verm/$ver/$board
 	cp .config 			$wwwdir/$verm/$ver/$board/dot-config
 	case $board in
@@ -278,7 +264,10 @@ for board in $boards ; do
 	esac
 	cd ../../
 	rm update-build-$verm-$board.lock
-	) >update-build-$verm-$board.log 2>&1 
+	if [ "$ca_user" != "" -a "$ca_pw" != "" ] ; then
+		curl -u "$ca_user:$ca_pw" -d status="$tags New Build #$verm #$ver for #$board Boards http://$servername/$verm/$ver/$board" http://identi.ca/api/statuses/update.xml >/dev/null
+	fi
+	) >update-build-$verm-$board.log 2>&1
 	#&
 	#pid=$!
 	#echo $pid > update-build-$verm-$board.pid

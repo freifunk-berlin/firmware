13a176bae2d4a038560bc9b3a72d8059c6d1c610
André Gaul <gaul@web-yard.de>
2014-08-01 23:36:41 +0200

    [scripts] fix PWD

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,15 +6,15 @@ OPENWRT_SRC=${OPENWRT_SRC:-git://git.openwrt.org/openwrt.git}
 OPENWRT_COMMIT=${OPENWRT_COMMIT:-65f9fd0dc881f5759a79dddee5d689e320626609}
 NCPU=${NCPU:-$(cat /proc/cpuinfo | grep ^processor | wc -l)}
 
-PWD=$(pwd)
-OPENWRT_DIR=${PWD}/openwrt
+FIRMWARE_DIR=$(pwd)
+OPENWRT_DIR=${FIRMWARE_DIR}/openwrt
 
 # cleanup openwrt dir
-rm -f ${OPENWRT_DIR}/bin
+rm -f ${FIRMWARE_DIR}/bin
 if [ -d ${OPENWRT_DIR} ]; then
     cd ${OPENWRT_DIR}
-    git clean -dff && git fetch || exit 1
-    rm -rf.config patches feeds.conf
+    git clean -dff && git fetch && git reset --hard HEAD || exit 1
+    rm -rf .config patches feeds.conf
 else
     git clone ${OPENWRT_SRC} openwrt || exit 1
     cd ${OPENWRT_DIR}
@@ -24,18 +24,18 @@ fi
 git checkout --detach ${OPENWRT_COMMIT} || exit 1
 
 # link and apply patches
-ln -s ${PWD}/patches ${OPENWRT_DIR}
+ln -s ${FIRMWARE_DIR}/patches ${OPENWRT_DIR}/patches
 quilt push -a || exit 1
 
 # activate feeds
-ln -s ${PWD}/feeds.conf ${OPENWRT_DIR}
+ln -s ${FIRMWARE_DIR}/feeds.conf ${OPENWRT_DIR}
 ./scripts/feeds update && ./scripts/feeds install -a || exit 1
 
 # copy and update (if necessary) .config
-cp ${PWD}/configs/${ARCH}.config ${OPENWRT_DIR}/.config
+cp ${FIRMWARE_DIR}/configs/${ARCH}.config ${OPENWRT_DIR}/.config
 yes | make oldconfig || exit 1
 
 # build!
 make -j${NCPU} IGNORE_ERRORS=m || exit 1
 
-ln -s ${OPENWRT_DIR}/bin ${PWD}/bin
+ln -s ${OPENWRT_DIR}/bin ${FIRMWARE_DIR}/bin

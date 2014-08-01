b09b854567bef026d503291f33a59643a2b1e241
André Gaul <gaul@web-yard.de>
2014-08-01 21:20:16 +0200

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,25 @@ OPENWRT_SRC=git://git.openwrt.org/openwrt.git
 OPENWRT_COMMIT=f5018fd2183cd92fd67255c220314cae813cce63
 
 # cleanup openwrt dir
-rm -f openwrt/patches openwrt/feeds.conf openwrt/.config
+if [ -d openwrt ]; then
+    cd openwrt
+    git clean -d && git pull || exit 1
+else
+    git clone ${OPENWRT_SRC} openwrt || exit 1
+    cd openwrt
+fi
 
-git clone ${OPENWRT_SRC} openwrt && \
-    ln -s patches openwrt/ && \
-    ln -s feeds.conf openwrt/ && \
-    ln -s configs/${ARCH}.config openwrt/.config
+# link patches, feeds and config
+ln -s ../patches .
+ln -s ../feeds.conf .
+ln -s ../configs/${ARCH}.config .config
 
-cd openwrt
-./scripts/feeds update
-./scripts/feeds install -a
-yes | make oldconfig
-make -j$(cat /proc/cpuinfo | grep ^processor | wc -l) IGNORE_ERRORS=m
+# activate feeds
+./scripts/feeds update && ./scripts/feeds install -a || exit 1
+
+# update .config if necessary
+yes | make oldconfig || exit 1
+
+# build!
+make -j$(cat /proc/cpuinfo | grep ^processor | wc -l) IGNORE_ERRORS=m || exit 1
 

d80f18171c0f2d236080a97c7f98005c5dc40a65
André Gaul <gaul@web-yard.de>
2014-08-01 22:35:10 +0200

    [scripts] apply patches w/ quilt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ARCH=ar71xx
-OPENWRT_SRC=git://git.openwrt.org/openwrt.git
-OPENWRT_COMMIT=65f9fd0dc881f5759a79dddee5d689e320626609
+# default parameters
+ARCH=${ARCH:-ar71xx}
+OPENWRT_SRC=${OPENWRT_SRC:-git://git.openwrt.org/openwrt.git}
+OPENWRT_COMMIT=${OPENWRT_COMMIT:-65f9fd0dc881f5759a79dddee5d689e320626609}
+NCPU=${NCPU:-$(cat /proc/cpuinfo | grep ^processor | wc -l)}
 
 # cleanup openwrt dir
+rm -f bin
 if [ -d openwrt ]; then
     cd openwrt
     git clean -df && git fetch || exit 1
@@ -17,19 +20,19 @@ fi
 # checkout specified commit
 git checkout --detach ${OPENWRT_COMMIT}
 
-# link patches
+# link and apply patches
 ln -s ../patches .
+quilt push -a || exit 1
 
 # activate feeds
 ln -s ../feeds.conf .
 ./scripts/feeds update && ./scripts/feeds install -a || exit 1
 
-# copy config
+# copy and update (if necessary) .config
 cp ../configs/${ARCH}.config .config
-
-# update .config if necessary
-#yes | make oldconfig || exit 1
+yes | make oldconfig || exit 1
 
 # build!
-make -j$(cat /proc/cpuinfo | grep ^processor | wc -l) IGNORE_ERRORS=m || exit 1
+make -j${NCPU} IGNORE_ERRORS=m || exit 1
 
+ln -s bin/ ../bin

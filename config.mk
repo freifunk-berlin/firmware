1b4dd817feed20b0ee419e76a4fb10f70e4335a0
Matthias <freifunk@3dfxatwork.de>
2014-08-14 21:07:04 +0200

    set ar71xx config add luci and openwrt patches set git hashes for openwrt and feeds

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 TARGET=ar71xx
-OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
-OPENWRT_COMMIT=77f0a1b2a845382d3c3a907020bf1a9eca1fae35
+OPENWRT_SRC=git://git.openwrt.org/openwrt.git
+OPENWRT_COMMIT=cfff55dd33b63098efd904936c802411ede39c44
 MAKE_ARGS="IGNORE_ERRORS=m"

dad9fe075b8a024a7be343776660e73992485144
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-08-30 01:24:21 +0200

    [feeds.conf]: update openwrt revision

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 TARGET=ar71xx
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
-OPENWRT_COMMIT=fef411a8e349146406031b39acc0ac15edf72d83
+OPENWRT_COMMIT=d3b76e0a58c78dc7087cf31270d7f9b18831681b
 MAKE_ARGS=

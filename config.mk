e5737d75636bf6d3bf6d0a73f34426ea2a99bb61
Nico Geyso <nico.geyso@fu-berlin.de>
2014-09-16 16:27:08 +0200

    Update OpenWRT version
    fixes the follwing bugs
    * ath9k regression bug
    * sdk problems

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 TARGET=ar71xx
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
-OPENWRT_COMMIT=394af9ce72f20095fd637eed45d0e84869917f20
+OPENWRT_COMMIT=cebcd32fe8d653fb6679661838e73eccb29e9f72
 MAKE_ARGS=

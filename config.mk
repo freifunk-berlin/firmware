f42dc9c26ded1aee32b8af316e97498653460572
Nico Geyso <nico.geyso@fu-berlin.de>
2014-09-23 14:24:01 +0200

    rename package list 'minimal' to 'vpn'

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # default parameters for Makefile
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=minimal
+PACKAGES_LIST_DEFAULT=vpn
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
 OPENWRT_COMMIT=cebcd32fe8d653fb6679661838e73eccb29e9f72
 MAKE_ARGS=

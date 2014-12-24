4801f87949afab60c171613c331738c4388872cb
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-12-24 13:41:26 +0100

    Update OpenWrt revision to 43770.
    Includes a fix for:
    
    https://github.com/freifunk-berlin/firmware/issues/160

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=vpn
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
-OPENWRT_COMMIT=83a67bc701f9c8f750519f9310c2e0992a339f89
+OPENWRT_COMMIT=331ecb083c3e7409bfa9c4d6d2e3db06dede3dfd
 MAKE_ARGS=

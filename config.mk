4fb186f65f9d179f3ef4dc1c197b05b02146b5bf
Philipp Borgers <borgers@mi.fu-berlin.de>
2015-02-02 02:14:55 +0100

    config.mk: Update OpenWrt revision to 44162
    The openwrt-package-openvpn-cipher-none-regression.patch is obsolete because
    it's now included upstream:
    
    http://git.openwrt.org/?p=14.07/openwrt.git;a=commit;h=c2a8bebec3bbb7dbe9d3e9fa50614dab230eac6e

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=vpn
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
-OPENWRT_COMMIT=331ecb083c3e7409bfa9c4d6d2e3db06dede3dfd
+OPENWRT_COMMIT=7d01d821b05b26304ab8f8fc148e690cb5ffd8b3
 MAKE_ARGS=

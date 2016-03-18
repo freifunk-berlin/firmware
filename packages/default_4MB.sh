6df20afe0d99416030ba13afab59022075fa5e65
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-03-18 03:17:54 +0100

    packages/default_4MB: remove opkg and usign
    - on these 4MB units it's very unlikely, that someone will/can install packages
    - remove opkg status-files and config
    - manually remove usign as this would be included into image for some reason

--- packages/default_4MB.sh
+++ packages/default_4MB.sh
@@ -6,3 +6,11 @@
 
 echo "Deleting OLSR i18n files..."
 rm -vf usr/lib/lua/luci/i18n/olsr.*
+echo "deleting opkg status-files ..."
+rm -rf usr/lib/opkg
+rm -rf etc/opkg*
+# as this will be included into image for some reason, even it's
+# not listed for inclusion
+echo "manually removing usign ..."
+rm usr/bin/usign
+rm usr/bin/signify

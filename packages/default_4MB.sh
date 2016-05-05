1d2d2306ced25a6fe3d0891a364d6e22ef521706
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-05-05 16:36:31 +0200

    default_4MB: use "cmp" to check for local modification
    - as suggested in https://github.com/freifunk-berlin/firmware/pull/341#issuecomment-216074670
    - this reduces the filesize
    - no additional binary required

--- packages/default_4MB.sh
+++ packages/default_4MB.sh
@@ -18,32 +18,23 @@ add_opkg_fix_conffiles()
 {
 	local filelist="$1"
 
-	# find the separator line (### sha256 filelist ###) and check all files listed there
-	# format is: "file" "sha256", as extracted from original /usr/lib/opkg/status
-	content_separator_line=`sed -n '/^### sha256 filelist ###/=' /lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh`
+	# find the separator line (### opkg conffiles ###) and check all files listed there
+	# it's just the list of conffiles extracted from opkg-/status during image-creation
+	content_separator_line=`sed -n '/^### opkg conffiles ###/=' /lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh`
 	let content_separator_line++
 	# iterate over all lines after "separator line"
 	tail -n +$content_separator_line /lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh | \
-		while read line; do
-			file=`echo $line | awk '{ print $1 }'`
-			file_has_changed ${file} && echo ${file} >> $filelist
+		while read filename; do
+			file_has_changed ${filename} && echo ${filename} >> $filelist
 		done
 }
 
 file_has_changed() {
-	if [ ! -x /usr/bin/sha256sum ]; then
-		echo "sha256sum command missing"
-		return 0
-	fi
-
-	# "-m 1" as sometimes files are listed twice in the status-file
-	confsha=`grep -m 1 "$1" /lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh | awk '{ print $2 }'`
-	filesha=`sha256sum "$1"`
-	# ${filesha:0:64} --> use 64 digits of sha256-output (cut off filename)
-	if [ ${filesha:0:64} != ${confsha} ]; then
-		return 0
-	else
+	if cmp -s "$@" "/rom/$@"; then
 		return 1
+	else
+		# modified
+		return 0
 	fi
 }
 
@@ -52,14 +43,14 @@ sysupgrade_init_conffiles="$sysupgrade_init_conffiles add_opkg_fix_conffiles"
 # to exit before going down to the filelist
 return 0
 
-### sha256 filelist ###
+### opkg conffiles ###
 KEEPLIST
 
 # this is run during firmware-image creation
 # add all conffiles known to opkg
 echo "adding opkg-conffiles to sysupgrade-hook"
 for file in `cat usr/lib/opkg/info/*.conffiles`; do
- grep ${file} usr/lib/opkg/status >>lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh
+ grep ${file} usr/lib/opkg/status | awk '{ print $1 }' >>lib/upgrade/freiunk-berlin_no-opkg-info-on-4mb-workaround.sh
 done
 
 echo "deleting opkg status-files ..."

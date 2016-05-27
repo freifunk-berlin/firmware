1b0c6bd93dbe317c8be08516b610d8a9635ebc97
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-05-27 23:19:09 +0200

    config.mk: remove optional ENV "REPOPATH"

--- config.mk
+++ config.mk
@@ -4,7 +4,6 @@ TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=default backbone
 OPENWRT_SRC=git://git.openwrt.org/15.05/openwrt.git
 OPENWRT_COMMIT=64e116779c0f7da6d98068b8e7c50f528c8a91f2
-REPOPATH=http://buildbot.berlin.freifunk.net/buildbot/unstable/%T/928/packages/
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
 BUILDTYPE=unstable

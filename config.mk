f1b56bfeb6fc1568462a3d7e53ee07d73fb7c3e2
Alexander Couzens <lynxis@fe80.eu>
2016-06-05 04:17:48 +0200

    append subtarget name to all platforms
    When subtarget support was introduce, it was only used for target ar71xx_mikrotik
    and none of the old targets e.g. ar71xx was renamed including it's subtarget.
    Every platform has a subtarget, even when it's generic.
    Rename those platform into $TARGET_$SUBTARGET
    LEDE requires to know the subtarget, because it has a different bin/ directory
    layout. It uses /bin/$TARGET/$SUBTARGET/foobar.sysupgrade.squashfs.bin
    
    ar71xx -> ar71xx_generic
    mpc85xx -> mpc85xx_generic
    ramips -> ramip_mt7620
    x86 -> x86_generic
    
    Signed-off-by: Alexander Couzens <lynxis@fe80.eu>

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
-TARGET=ar71xx
+TARGET=ar71xx_generic
 PACKAGES_LIST_DEFAULT=default backbone
 OPENWRT_SRC=git://github.com/openwrt/openwrt.git
 OPENWRT_COMMIT=1b6dc2e48ce654a004a7d0b98d7070a515424595

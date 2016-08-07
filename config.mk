2a59844cc24c4630b3547db7eac2ccedec8e217c
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-08-07 14:12:26 +0200

    Makefile: workaround to allow releasing
    since 34d519c there is the git-commithash in the firmware-filename,
    which broke the current automatic deploymentof releases from buildbot.
    This workaround is a hack to restore the old filename-scheme to allow
    releasing of kathleen-0.2.0. In parallel we should look for a clean way
    for master-branch
    
    setting BUILDTYPE on config.mk decides on the naming-scheme in the
    Makefile. Valid values are "unstable" or "release".

--- config.mk
+++ config.mk
@@ -5,3 +5,5 @@ PACKAGES_LIST_DEFAULT=default backbone
 OPENWRT_SRC=git://github.com/openwrt/openwrt.git
 OPENWRT_COMMIT=9a1fd3e313cedf1e689f6f4e342528ed27c09766
 MAKE_ARGS=
+#BUILDTYPE - unstable / release
+BUILDTYPE=unstable

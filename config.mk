207847b285a42cde67c9d9ac476c086a778028ca
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-11-27 03:44:31 +0100

    Kathleen 0.2.0 release build

--- config.mk
+++ config.mk
@@ -6,4 +6,4 @@ OPENWRT_SRC=git://github.com/openwrt/openwrt.git
 OPENWRT_COMMIT=1b6dc2e48ce654a004a7d0b98d7070a515424595
 MAKE_ARGS=
 #BUILDTYPE - unstable / release
-BUILDTYPE=unstable
+BUILDTYPE=release

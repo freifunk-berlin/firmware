eab03437c44f8fa19e7081fb898ae6310c977681
Sven Roederer <devel-sven@geroedel.de>
2017-02-23 00:57:46 +0100

    config, feeds: update to LEDE 17.01.0 release

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=25b7295617cc6b2e317ef970d6d8d04ada49822c
+LEDE_COMMIT=ac733df99c78f54b4cf9a8710f70a506f73e66fc
 MAKE_ARGS=

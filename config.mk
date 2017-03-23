a2bee315133d8685f9f181b395b27a57ff9b0749
Sven Roederer <devel-sven@geroedel.de>
2017-03-23 21:13:17 +0100

    config,feeds: update LEDE, packages and LuCI
    LEDE 17.01-branch:
      * mac80211: sync with master branch as of 9edff13
      * libubox: update to the latest version
      * ubus: update to the latest version
      * mac80211: hwsim: select DRIVER_11AC_SUPPORT and DRIVER_11W_SUPPORT
      * hostapd: fix feature indication
      * procd: update to the latest version
      * px5g: replace px5g-standalone with a statically linked variant of px5g-mbedtls
      * swconfig: Bugfix switch_port uci option parsing
      * kernel: update kernel 4.4 to 4.4.59
      * mbedtls: update to version 2.4.2
      * split-up of kmod-mt76
    
    feed LuCI:
      * luci-base: backport cbi datatype changes
      * luci-mod-admin-full: new hostapd functionality for 80211w/r detection
      * luci-app-firewall: adjust drop_invalid default
    
    feed: packages
      * no relevant changes for us

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=ac733df99c78f54b4cf9a8710f70a506f73e66fc
+LEDE_COMMIT=7099bb19b50d74d3a400aa3a60e0b7ba0157bc1e
 MAKE_ARGS=

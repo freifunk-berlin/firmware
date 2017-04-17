6606e57cd03571aba90e89c414e700ee15a83385
Sven Roederer <devel-sven@geroedel.de>
2017-04-17 22:57:15 +0200

    config.mk: update to LEDE v17.01.1
    * kernel: update kernel 4.4 to 4.4.61
    * opkg: switch to LEDE fork (#120, #551, #571)
    * mac80211: add support for Marvell 802.11n/802.11ac SDIO Wireless cards

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=ac733df99c78f54b4cf9a8710f70a506f73e66fc
+LEDE_COMMIT=48461b5abc382b26edd0d2637f0f7b952bed90ba
 MAKE_ARGS=

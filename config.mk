072459ea8819d27ae579b33993076a26c42b055e
Sven Roederer <devel-sven@geroedel.de>
2017-05-26 11:04:19 +0200

    config: update LEDE to 17.01-HEAD
    also add ath10k-firmware-qca988x for ar71xx-generic target,
    as since LEDE-commit 0e31ce730 the ath10k-option is not
    selecting the qca988x as dependency
    
    LEDE:
    fe5e343 usbmode: update usb-modeswitch-data to 20170205
    757353c firewall: resync with master
    4bd3b8f mac80211, hostapd: always explicitly set beacon interval
    e194e1b hostapd: add legacy_rates option to disable 802.11b data rates.
    784ceba treewide: select ath10k firmware explicit
    0e31ce7 ath10k-firmware: do not select the qca988x by default
    e02b12c kernel: update kernel 4.4 to 4.4.70
    d1a0fc3 binutils: fix build with host gcc < 4.9
    d179aa8 util-linux: fix build with uclibc
    dd19a41 dropbear: bump to 2017.75
             CVE-2017-9078, Fix information disclosure with ~/.ssh/authorized_keys symlink
    51db1f5 samba: fix CVE-2017-7494
    74100f3 bcm53xx: add support for TP-LINK Archer C5 V2
    dfe2cea firmware-utils: tplink-safeloader: add support for Archer C5 V2
    9423cf3 om-watchdog: add support for Teltonika RUT5xx (ramips)
    8011215 ar71xx: enable nand-utils in the mikrotik subtarget to ensure it makes it to initramfs
    aba1b3c openvpn: update to v2.4.2
    98491a9 openvpn: add extra respawn parameters
    bc58099 openvpn: move list of params and bools to a separate file
    d8cfeba dnsmasq: support dhcp_option config as a list
    8619683 ramips: add factory firmware for Tp-Link C20i/C50
    79cd141 ramips: enable ramdisk for mt7621
             factory image for ubnt er-x missing
    eb11207 mac80211: rt2800: fix mt7620 E2 channel registers
    64fa4ea mac80211: rt2800: fix mt7620 vco calibration registers
    820a396 mac80211: rt2x00: fix MT7620 LNA gain and VCO-after-ALC
    5b91d2b mac80211: rt2x00: import upstream changes and rebase our patches
    ab7087e rt2x00: mt7620: make fixes requested upstream
    5ac51ad ath9k: fix power limits on init
    a972879 ath: do not apply broken power limits with ATH_USER_REGD
    503e496 odhcpd: update to version 2017-04-28 (FS#595)
    49ce6d0 ramips: add support for Sanlinking D240
    6aa0a85 ramips: remove DT pcie nodes for GL-MT300A/N
    a666236 ramips: add ip17xx support to WLI-TX4-AG300N
    0405851 ramips: fix EX2700 wireless mac
    1aee42c ramips: add support for Netgear WN3000RPv3
    846457f ramips: fix mac address of miwifi-mini
    af1d1eb x86: enable 4G high memory support for generic (32bit) subtarget
    2bc8d5e ubox: bump to version 2017-03-10
    3dbc417 ar71xx: add TP-LINK TL-WR841N/ND v12 image

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=48461b5abc382b26edd0d2637f0f7b952bed90ba
+LEDE_COMMIT=fe5e3439335879c205cf090e35939cd5c68d04ea
 MAKE_ARGS=

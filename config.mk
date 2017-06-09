436df00f7867c9d2e0f86934e3c95e7cbd977b5f
Sven Roederer <SvenRoederer@users.noreply.github.com>
2017-06-09 02:21:06 +0200

    config: update LEDE-17.01 HEAD
    this updates to LEDE-17.01 HEAD of 8 Jun 2017 23:02:37
    
    * base-files: network.sh: fix a number of IPv6 logic flaws
    * base-files: network.sh: properly report local IPv6 addresses
    * kernel: update kernel 4.4 to 4.4.71
    * dnsmasq: bump to 2.77
    * and others

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=fe5e3439335879c205cf090e35939cd5c68d04ea
+LEDE_COMMIT=65eec8bd5f6337956b972d07fde49eb5db9cb4a0
 MAKE_ARGS=

cf1fa38dc22b389751d3794c297f67460ace5b0b
Sven Roederer <devel-sven@geroedel.de>
2017-07-10 08:44:07 +0200

    config: update to LEDE-17.01 HEAD (Tue, 25 Jul 2017)
    f6907dc image: fix ar71xx legacy images
    8fbef4b imx6: fix DualLite/Solo GW551X board detection
    82b20d7 procd: backport kernel watchdog start/stop support
    c047c34 x86: add missing kernel config symbols to Geode target
    05643bd x86: enable ACPI support for the Geode subtarget
    699e312 dnsmasq: backport patch fixing DNS failover (FS#841)
    d0ec502 ar71xx: set US region code for TP-Link TL-WR710N v1 image
    7896d7b fstools: backport fixes from master branch
    870ca0d ar71xx: fix switch port mapping for TP-Link TL-WR74xN/D series
    823d35f kernel: netfilter: fix nf-nathelper(-extra) description

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=eff34695104da2d3e960841ff79b1cd590021acd
+LEDE_COMMIT=870ca0da7a55d35718f7218f80b7cdb3696ae35b
 MAKE_ARGS=

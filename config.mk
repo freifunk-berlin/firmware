9e7deaf193702f17dbab81d9f38395355f041639
Sven Roederer <devel-sven@geroedel.de>
2017-08-14 19:14:27 +0200

    config: update to LEDE 17.01 HEAD (Aug 17, 2017)
    * kernel: update kernel 4.4 to version 4.4.83
    * curl: fix CVE-2017-7407 and CVE-2017-7468
    * some fixes for Archer C50v1
    * ralink: fix rcu_sched stalls on mt7621
    * procd: update to latest git HEAD
    * dnsmasq: backport remove ping check of configured dhcp address
    * mbedtls: Re-allow SHA1-signed certificates
    * tcpdump: Update to 4.9.1

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=a5822dbd0f12054d04d1415a98ae995f5438ccd8
+LEDE_COMMIT=4a1b87aba448fae648bf17830379fde11060927b
 MAKE_ARGS=

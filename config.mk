d9ee9b97b7b7e1cd78ce3f28094cfc934355efec
Sven Roederer <devel-sven@geroedel.de>
2017-08-22 04:56:59 +0200

    packages: change "default" as notunnel-setup
    * rename current "default" flavor to VPN03
    * rename current "notunnel" flavor to default, to have this as minimal basis

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default notunnel tunnel-berlin backbone
+PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
 LEDE_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
 MAKE_ARGS=

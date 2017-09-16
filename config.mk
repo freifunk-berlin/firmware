d7dca8737789beafaa051ad17672e5f665b81de3
Sven Roederer <devel-sven@geroedel.de>
2017-09-16 19:20:50 +0200

    adapt to new packages for VPN03 and tunnel-berlin
    * build both avail tunnel-type defaults
    * as we have now 2 alterantive uplink-tunnels (VPN03, Tunnel-Berlin) include a specific type into the image
      * default flavor uses VPN03
      * new tunnel-berlin flavor uses tunnel-berlin

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default backbone
+PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
 LEDE_COMMIT=d0bf257c46aae0b0d2567c10d6c28c3b2d857f08
 MAKE_ARGS=

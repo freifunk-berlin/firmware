7739d59bfe07d68eb1a777d77827b85f30ff1c9b
Sven Roederer <devel-sven@geroedel.de>
2017-10-04 20:15:42 +0200

    config: update to HEAD of LEDE-17.01 branch (3. Oct 2017)
    this matches LEDE-17.01.3
    
    * base-files: fix wan6 interface config generation for pppoe
    * tcpdump: bump to 4.9.2 and fix some CVEs
    * openvpn: add "extra-certs" option
    * kernel: update 4.4 to 4.4.89
    * mbedtls: update to 2.6.0 CVE-2017-14032
    * curl: fix security problems
    * base-files: create /etc/config/ directory
    * dnsmasq: bump to v2.78
    * uhttp: update to latest version

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=97ebdf93a330ef1d7ff1cf6989cfb92ac2f5a386
+LEDE_COMMIT=d0bf257c46aae0b0d2567c10d6c28c3b2d857f08
 MAKE_ARGS=

3cf833ccd0b535dac0c97d51e614341369ab3061
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-10-16 22:46:27 +0200

    config: update LEDE to HEAD of 17.01-branch (Oct 18, 2017)
    * kernel: bump 4.4 to 4.4.93 for 17.01
    * mountd: bump to git HEAD version (fixes SIGSEV crashes)
    * hostapd: backport extra changes related to KRACK
    * hostapd: add wpa_disable_eapol_key_retries option
    * mac80211: Update wireless-regdb to master-2017-03-07
    ** backport kernel fix for CVE-2017-13080
    * wireguard: add wireguard to base packages
    ** version bump to 0.0.20171017
    * kernel: bump 4.4 to 4.4.92
    * hostapd: merge fixes for WPA packet number reuse with replayed messages and key reinstallation
    * x86: fix Xen-support
    ** x86/64: add xen DomU support
    ** x86: Fix xen serial console by removing conflicting PATA driver
    ** x86/generic: use HIGHMEM64G instead of HIGHMEM4G to fix PAE and Xen
    ** this upstreams the patches we used before
    * mt76: sync with version 878456c from master
    * toolchain/gdb: update to version 8.0.1

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=d0bf257c46aae0b0d2567c10d6c28c3b2d857f08
+LEDE_COMMIT=373fa54d355479c9be13af13f1e71d1b829fc5d0
 MAKE_ARGS=

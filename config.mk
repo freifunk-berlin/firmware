07e9d690e4696ab3e8e17983ca1326164668829d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-10-29 02:05:19 +0100

    config: update to HEAD of LEDE-17.01 (Oct 25, 2017)
    * opkg: bump to 2017-10-23 (lede-17.01)
    * ramips: fix default usb support for nexx wt3020-8M
    * dnsmasq: restore ability to include/exclude raw device names

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=373fa54d355479c9be13af13f1e71d1b829fc5d0
+LEDE_COMMIT=367b4563b4cd466188bd4c8cf24ff0a125029c1c
 MAKE_ARGS=

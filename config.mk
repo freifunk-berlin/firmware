72df4e4b0f8355e2dee5a8b9cb46e15d7f6fdbd0
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-11-09 17:27:42 +0100

    update to HEAD of LEDE-17.01 (8 Dec 2017)
    b41a2e6 opkg: bump to version 2017-12-08
    f5f5f58 hostapd: backport fix for wnm_sleep_mode=0
    19ebc19 hostapd: Expose the tdls_prohibit option to UCI
    3590316 dnsmasq: backport infinite dns retries fix
    060b7f1 curl: apply CVE 2017-8816 and 2017-8817 security patches
    4b5861c mt76: update to the latest version
    f19d47f tools: patch various gnu tools for macOS 10.13
    e5a10bc samba36: backport an upstream fix for an information leak (CVE-2017-15275)
    d77fe92 ramips: backport MT7628 pinmux fixes
    9601e6a ramips: add missing reset button for Nexx WT1520
    0946ec0 wireguard: bump to snapshot 20171127
    7f3dab2 kernel: bump 4.4 to 4.4.102
    d3f40aa wireguard: bump to 20171122
    7ec6394 ramips: fix Planex CS-QR10 device packages
    6cfa7e5 ramips: fix DCH-M225 support
    e626942 dnsmasq: load instance-specific conf-file if exists
    d64c0e5 rpcd: update to version 2017-11-12
    ecaad8b brcm47xx: fix switch port mapping on D-Link DIR-330
    d851d7f wireguard: fix portability issue
    8751bd7 wireguard: move to kernel build directory
    ed571c1 wireguard: bump to 0.0.20171111
    c9fb48a procd: update to latest git HEAD (fixes and improvements)
    cda8ec7 openssl: update to 1.0.2m
    4217541 brcm47xx: fix switch port mapping on Asus RT-N12 and RT-N16 models
    95824b9 rpcd: update to the latest version from 2017-11-09
    792559f mountd: bump to git HEAD version (optimization fixes)
    a0ef1c4 functions.sh: fix default_postinst function
    6b6578f wireguard: version bump to 0.0.20171101
    9740523 ar71xx: fix LED config for DIR-869 A1
    bdf19ee ipq806x: nbg6817: sync MAC addresses to the upstream values
    2aff2ad ipq806x: nbg6817: add kmod-fs-ext4 to device packages
    63f6408 uclient: update to the latest version, fixes fetch of multiple files
    3eae19a ramips: fix Youku-YK1 support
    8a48a53 tools/squashfs4: include sysmacros.h explicitly
    8406e50 tools/squashfs: include sysmacros.h explicitly
    96dbf59 tools/mtd-utils: include sysmacros.h explicitly
    d2fd641 tools/findutils: include sysmacros.h explicitly

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=367b4563b4cd466188bd4c8cf24ff0a125029c1c
+LEDE_COMMIT=b41a2e646e71d9a483fa3d770a0f53a33eea696c
 MAKE_ARGS=

dd88eee80fe97024f5cfa9b9d2f5d1b68ac02a4d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-11-13 00:07:19 +0100

    update to HEAD of LEDE-master (30 Dec 2017)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=098afa1e1b29918c8d9d4a675464be896aadc7a1
+OPENWRT_COMMIT=7aa15953e1b60033fb1390b97fd6fe42daced738
 SET_BUILDBOT=env
 MAKE_ARGS=

8e7eee08b57737ff1b483a1f9dc1ee1ad6917236
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-12-08 20:16:31 +0100

    Makefile: add setting "SET_BUILDBOT"
    this controls the detection if we run on a buildbot and if we
    shall honor the Environment "IS_BUILDBOT"
    valid parameters:
    - no:  "IS_BUILDBOT" will be ignored
    - yes: "IS_BUILDBOT" will be interpreted as set to "yes"
    - env: "IS_BUILDBOT" is taken from the env-variables (yes / no)
    
    To make this work effectively at least https://github.com/freifunk-berlin/buildbot/pull/42
    should be implemented on our Buildbots, to set "IS_BUILDBOT".

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
+SET_BUILDBOT=env
 LEDE_SRC=https://git.lede-project.org/source.git
 LEDE_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
 MAKE_ARGS=

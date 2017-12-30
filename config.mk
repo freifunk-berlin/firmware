97b86bec79efc59b73f99760270d8e9e0d39d3eb
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-12-30 15:11:05 +0100

    update to HEAD of LEDE-17.01 (30 Dec 2017)
    ee55629 kernel: bump 4.4 to 4.4.108 for 17.01
    a190802 base-files: fix logic when to show failsafe banner
    1c92998 base-files: set FAILSAFE in /etc/profile when /tmp/.failsafe exists
    2603c85 wireguard: bump to 20171221

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=ceea0ac25d987de1f84376405d38d43337d31cf5
+LEDE_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
 MAKE_ARGS=

613b84d21535245172e5ae7061909fd5bd2eee4d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-01-03 01:19:49 +0100

    Revert "add support for LEDE" to follow remerge with OpenWRT
    This reverts relevant things from commit d888c21df67c5f2e424fa2955849890ac4a66be8,
    but keeps the same logic as LEDE has. As the remerge is taking the OpenWRT name
    and using the LEDE-code (only seen from the code perspective).
    See also http://lists.infradead.org/pipermail/lede-dev/2018-January/010544.html

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
-LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
+OPENWRT_SRC=https://github.com/openwrt/openwrt.git
+OPENWRT_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
 MAKE_ARGS=

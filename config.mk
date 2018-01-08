b647729cde575b60907b16ed120dfd3c1d9586af
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-01-08 23:17:21 +0100

    update to HEAD of OpenWRT-master (23. Feb 2018)
    * ar71xx: disable 40Mhz refclk for QCA953x
    * ramips: enable power LED and second uart on GL-MT300N-V2
    * enable TL_WR842v2 again
    * 802.11s use 1Mbps for compatibility
    * use gcc 7.3.0

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=7aa15953e1b60033fb1390b97fd6fe42daced738
+OPENWRT_COMMIT=2805402f868871a178a80198b990675bcc433699
 SET_BUILDBOT=env
 MAKE_ARGS=

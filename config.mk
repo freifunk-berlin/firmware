47db7e2934520b7422a8b54cf86197b328e1eef0
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-01-16 09:09:10 +0100

    update to HEAD of LEDE-17.01 (22 Jan 2018)
    * kernel: update kernel 4.4 to version 4.4.112
    * procd: update to latest git HEAD
    * rpcd: backport version 2017-12-07 from master
    * uci: update to HEAD of lede-17.01 branch
    
    full log:
    
    d5278cc48b kernel: bump 4.4 to 4.4.112 for 17.01
    2ae0741f3b dnsmasq: backport validation fix in dnssec security fix
    58d60bd283 dnsmasq: backport dnssec security fix for 17.01
    d626aa005b mountd: bump to git HEAD version
    f0336975be kernel: bump 4.4 to 4.4.111 for 17.01
    fb6f21c657 kmod-sched-cake: bump to latest cake bake for 17.01
    2e8a3bb35f ar71xx: Netgear WNR2000v4: do not include USB packages [17.01]
    3fa86282fa build: fix restoring /etc/opkg with PER_DEVICE_ROOTFS
    987a7e3175 ramips: fix lenovo newifi-y1 switch and LED config
    dbb5ffaed5 ramips: firewrt: indicate boot status via LED
    c3cdc53164 ag71xx: Fix rx ring buffer stall on small packets flood on qca956x and qca953x.
    e07ee06aad ar71xx: QCA956X: add missing register
    7bc25dfa63 mvebu: fix mvneta build with Linux 4.4.110
    541a1a7ff5 lantiq: activate noise margin delta for VDSL too
    dea8979a81 Lantiq: make possible to tweak DSL SRN from UCI
    9934231670 libubox: update to latest lede-17.01 git HEAD
    7f5a040359 kernel: update kernel 4.4 to version 4.4.110
    ddedcb19e5 brcm47xx: relocate the stack in loader
    da43069f5b brcm47xx: relocate loader to higher address
    f19416ae9d fstools: backport fix from master branch
    55c23e44f4 procd: update to latest git HEAD
    ffbbcc9b34 brcm47xx: image: build firmware for Asus WL-500g Deluxe
    0426596453 Revert "iptables: fix nftables compile issue (FS#711)"
    da126d557c iptables: fix nftables compile issue (FS#711)
    473e994571 rpcd: backport version 2017-12-07 from master
    b833944eb2 uci: update to HEAD of lede-17.01 branch

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
+OPENWRT_COMMIT=d5278cc48b5b08b7aa09c2c3854cf16e5ceae408
 MAKE_ARGS=

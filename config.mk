6c05c5e052c9d76b815bd2d6834b59d757454651
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-03-06 12:41:45 +0100

    remove VPN03 flavor
    as this service will be shut down

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
+PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=606cea8f1b2d1017e238890157a431ccf4988071
 SET_BUILDBOT=env

d9efc443df8cff516e9eef652eca2a7fcdbd7b8d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-03-19 09:54:42 +0100

    update to HEAD of LEDE-17.01 (18. Mar 2018)
    * kernel 4.4.120
    * enable loading of microcode (spectre / meltdown)
    * mbedtls: update to version 2.7.0
    
    60f8d388c6 kernel: merge a pending fix for HFSC warnings/slowdowns (fixes FS#1136)
    f609913b5c mbedtls: update to version 2.7.0
    58a95f0f8f tools/e2fsprogs: fix building on a glibc 2.27 host
    9bdea6a296 generic: revert broken LED core patch
    9b0a4bafbc base-files: tune fragment queue thresholds for available system memory
    b47094ce96 include/package-defaults.mk: fix default Build/Prepare with empty ./src
    75be005e8b include/rootfs.mk: retain list of conffiles with CONFIG_CLEAN_IPKG
    696c6325a3 include/rootfs.mk: do not remove opkg prerm scripts during rootfs preparation
    17c0362178 base-files: sysupgrade: do not rely on opkg to list changed conffiles
    2ae9ebf374 kernel: bump 4.4 to 4.4.120 for 17.01
    571d3def6b x86: add preinit hook to reload microcode
    681aaaf719 firmware: add microcode package for Intel
    c6314ee06f firmware: add microcode package for AMD
    222521d593 tools: add iucode-tool
    f7a6b6724a x86: enable microcode loading for Intel and AMD
    dfe620cb93 odhcpd: fix interop with wide DHCPv6 client (FS#1377)
    18c999a6ff base-files: fix off-by-one in counting seconds for factory reset
    92ea65b36a sunxi: disable LPAE to allow kernel to run on A13
    7dcbe0e22d bcm53xx: fix fallback code for picking status LED
    4db583b9c2 mountd: update to the latest version from 2018-02-26
    01d7a5d7de perf: restrict libunwind dependency to archs that actually support libunwind
    b345cc2489 libunwind: fix build with musl on PPC

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=788312ca59c57f1cfc4737378526ff1c2a1c8374
+OPENWRT_COMMIT=60f8d388c69e0faddbf1c3033cdcb74b38fe3694
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

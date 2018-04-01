9799c7ef5a968e2b28621107111a3164134b1163
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-04-01 23:52:02 +0200

    update to HEAD of LEDE-17.01 (19 Apr 2018)
    efb6ca1896 base-files: /lib/functions.sh: ignore errors in insert_modules
    b5ba01a0d3 fstools: update to latest lede-17.01 branch
    a9b6077402 kernel: bump kernel 4.4 to 4.4.126 for 17.01
    09d95e44fc mbedtls: change libmbedcrypto.so soversion back to 0
    4673a0bffc kernel: mtd: bcm47xxpart: improve handling TRX partition size
    6577fe2198 ar71xx: sysupgrade: improve CPE/WBS 210/510 validation, add new metadata offset
    afca23558a kernel: backport commit reverting genirq patch causing regressions
    ba502a49a7 intel-microcode: update to 20180312
    5a9b101543 brcm47xx: add Luxul XAP-1500 and XWR-1750 WiFi LEDs
    fe3db683f9 mac80211: brcmfmac: add support for BCM4366E chipset
    3ca1438ae0 mbedtls: update to version 2.7.2
    c4b3829348 openssl: update to 1.0.2o
    bed0ee7cbf Kernel: bump 4.4 to 4.4.124 for 17.01
    fad29d2c3d mac80211: brcmfmac: backport commit dropping IAPP packets by default

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=60f8d388c69e0faddbf1c3033cdcb74b38fe3694
+OPENWRT_COMMIT=efb6ca189641aec64ba94f0d6d4e008fb2c1668b
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

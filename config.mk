a4fc571ac2b3af7733f2324fd772fbcb1edf4013
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-04-26 22:54:07 +0200

    update to HEAD of LEDE-17.01 (25. Apr 2018)
    23a638ebd1 brcm47xx: backport upstream patches for Netgear WNR1000 V3

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=efb6ca189641aec64ba94f0d6d4e008fb2c1668b
+OPENWRT_COMMIT=23a638ebd1fd7b9a38473efa819c0aaec8870822
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

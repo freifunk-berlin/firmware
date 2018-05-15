cfff78c938d2bd278cea7e679ac3331ffda05d0e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-05-15 19:40:33 +0200

    update OpenWrt to HEAD of 18.06 branch
    the upcoming release was branched now ....

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=606cea8f1b2d1017e238890157a431ccf4988071
+OPENWRT_COMMIT=dc7487885ef392efd547532181bec11a879c31b8
 SET_BUILDBOT=env
 MAKE_ARGS=

c6df85ca9f86ceae8ed5827bbf8b90b3dd8e06c3
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-05-19 00:01:09 +0200

    update to HEAD of LEDE-17.01 (18. May 2018)
    2c0cd47dab rpcd: update to lastest HEAD
    7b54d52a44 kernel: use accepted version of bcm47xxpart fix commit
    3c81d12ec3 bcm53xx: backport the first bunch of 4.18 BCM5301X patches
    b078753084 ar71xx: fix and improve ALFA Network Tube2H support
    e8b1af45f8 kernel: bump kernel 4.4 to 4.4.131 for 17.01
    999bb66b20 kernel: add missing in6_dev_put_clear call to an ipv6 network patch
    81573ea259 kernel: bump kernel 4.4 to 4.4.129 for 17.01
    afa8873887 gcc: gcc 6.3.0 fix comparison between pointer and integer
    eed9d40133 ar71xx: Ubiquiti Airmax M: add relocate-kernel to invalidate cache

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=23a638ebd1fd7b9a38473efa819c0aaec8870822
+OPENWRT_COMMIT=2c0cd47dab77dc5b8380cde89651642d0b406afe
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

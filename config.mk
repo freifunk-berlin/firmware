8fd7933367cb8db1cc33fbc1b02e628976546b3a
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-05-29 23:01:01 +0200

    update to HEAD of LEDE-17.01 (29 May 2018)
    * kernel 4.4.132
    * enable loading of microcode (spectre / meltdown)
    * mbedtls: update to version 2.7.2
    * openssl: update to 1.0.2o
    * fix Ubiquiti Airmax M boot problem
    
    4a38c0cad5 odhcpd: fix managed address configuration setting
    058a0b7552 apm821xx: Add default packages to NAND target
    6eec0e46ed wireguard: bump to 20180519
    5ad80ff24b wireguard: no longer need portability patch
    83089916dc wireguard: bump to 20180514
    6cc65b05f3 wireguard: Add support for ip6prefix config option
    0e3cc08496 wireguard: bump to 0.0.20180513
    4cb9af80dd wireguard: bump to 20180420
    9e5bed6652 wireguard: bump to 20180304
    57e773bd4e wireguard: bump to 20180202
    90d9df048b wireguard: bump to 20180118
    161d95f4c5 kernel: bump kernel 4.4 to 4.4.132 for 17.01
    2c0cd47dab rpcd: update to lastest HEAD
    7b54d52a44 kernel: use accepted version of bcm47xxpart fix commit
    3c81d12ec3 bcm53xx: backport the first bunch of 4.18 BCM5301X patches
    b078753084 ar71xx: fix and improve ALFA Network Tube2H support
    e8b1af45f8 kernel: bump kernel 4.4 to 4.4.131 for 17.01
    999bb66b20 kernel: add missing in6_dev_put_clear call to an ipv6 network patch
    81573ea259 kernel: bump kernel 4.4 to 4.4.129 for 17.01
    afa8873887 gcc: gcc 6.3.0 fix comparison between pointer and integer
    eed9d40133 ar71xx: Ubiquiti Airmax M: add relocate-kernel to invalidate cache
    23a638ebd1 brcm47xx: backport upstream patches for Netgear WNR1000 V3
    efb6ca1896 base-files: /lib/functions.sh: ignore errors in insert_modules
    b5ba01a0d3 fstools: update to latest lede-17.01 branch
    a9b6077402 kernel: bump kernel 4.4 to 4.4.126 for 17.01
    09d95e44fc mbedtls: change libmbedcrypto.so soversion back to 0
    4673a0bffc kernel: mtd: bcm47xxpart: improve handling TRX partition size
    6577fe2198 ar71xx: sysupgrade: improve CPE/WBS 210/510 validation, add new metadata offset
    afca23558a kernel: backport commit reverting genirq patch causing regressions
    ba502a49a7 intel-microcode: update to 20180312
    5a9b101543 brcm47xx: add Luxul XAP-1500 and XWR-1750 WiFi LEDs
    fe3db683f9 mac80211: brcmfmac: add support for BCM4366E chipset
    3ca1438ae0 mbedtls: update to version 2.7.2
    c4b3829348 openssl: update to 1.0.2o
    bed0ee7cbf Kernel: bump 4.4 to 4.4.124 for 17.01
    fad29d2c3d mac80211: brcmfmac: backport commit dropping IAPP packets by default
    60f8d388c6 kernel: merge a pending fix for HFSC warnings/slowdowns (fixes FS#1136)
    f609913b5c mbedtls: update to version 2.7.0
    58a95f0f8f tools/e2fsprogs: fix building on a glibc 2.27 host
    9bdea6a296 generic: revert broken LED core patch
    9b0a4bafbc base-files: tune fragment queue thresholds for available system memory
    b47094ce96 include/package-defaults.mk: fix default Build/Prepare with empty ./src
    75be005e8b include/rootfs.mk: retain list of conffiles with CONFIG_CLEAN_IPKG
    696c6325a3 include/rootfs.mk: do not remove opkg prerm scripts during rootfs preparation
    17c0362178 base-files: sysupgrade: do not rely on opkg to list changed conffiles
    2ae9ebf374 kernel: bump 4.4 to 4.4.120 for 17.01
    571d3def6b x86: add preinit hook to reload microcode
    681aaaf719 firmware: add microcode package for Intel
    c6314ee06f firmware: add microcode package for AMD
    222521d593 tools: add iucode-tool
    f7a6b6724a x86: enable microcode loading for Intel and AMD
    dfe620cb93 odhcpd: fix interop with wide DHCPv6 client (FS#1377)
    18c999a6ff base-files: fix off-by-one in counting seconds for factory reset
    92ea65b36a sunxi: disable LPAE to allow kernel to run on A13
    7dcbe0e22d bcm53xx: fix fallback code for picking status LED
    4db583b9c2 mountd: update to the latest version from 2018-02-26
    01d7a5d7de perf: restrict libunwind dependency to archs that actually support libunwind
    b345cc2489 libunwind: fix build with musl on PPC

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=788312ca59c57f1cfc4737378526ff1c2a1c8374
+OPENWRT_COMMIT=4a38c0cad5b2bfe37dd921dae04346f9a6397319
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

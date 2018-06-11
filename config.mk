094c796864622e1628e651ad7e998defbbf51962
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-11 20:52:06 +0200

    update to HEAD of LEDE-17.01 (11 Jun 2018)
    21f44e3389 map: add ealen as configurable uci parameter
    b6a1f43075 base-files: fix UCI config parsing and callback handling
    6734f32d91 mtd: add build hack to reintroduce shared mtd for older releases
    aaac9e82aa mtd: mark as nonshared to fix FS#484
    0fee4906d5 ustream-ssl: update to latest git HEAD
    f34a0756fd ustream-ssl: update to latest git HEAD
    98b9d8a3d3 mbedtls: Activate the session cache
    88ba88e5ad mbedtls: update mbedtls to version 2.7.3
    2328977fed kernel: bump kernel 4.4 to 4.4.135 for 17.01
    6e47c22268 ar71xx: Deactivate build of Netgear WNR2000v3
    9dd189d4ad mvebu: Add support for WRT3200ACM with new NAND flash
    adfd64ddd6 brcm47xx: add switch port mapping to Asus WL-500W

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=4a38c0cad5b2bfe37dd921dae04346f9a6397319
+OPENWRT_COMMIT=21f44e338976f54812a8f917e6648d8167a26936
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

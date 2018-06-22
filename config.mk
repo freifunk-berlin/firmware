d9ac02110dfce279041b58d2580d9d7cf8bba4f9
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-22 19:22:22 +0200

    use OpenWrt 18.06-rc1

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=25eb240f63adaa51882b7c9dee9a2e0b578b5ed1
+OPENWRT_COMMIT=13f64a1e597e00e7bc9cf73866533f668482c4fa
 SET_BUILDBOT=env
 MAKE_ARGS=

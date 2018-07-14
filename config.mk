928043b1c5928b8a0989c637ae5fed8efad39d2a
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-07-15 00:09:19 +0200

    update to HEAD of LEDE-17.01 (14 Jul 2018)
    38e704be71 kernel: bump kernel 4.4 to version 4.4.140
    aaecfecdcd kernel: bump kernel 4.4 to version 4.4.139
    b08003223a base-files: fix links in banner.failsafe
    71019a7605 ar71xx: fix 5 GHz Wi-Fi on NBG6716
    ba5c0a1dea Revert "base-files: fix UCI config parsing and callback handling"
    5c6a8a9cdb kernel: bump kernel 4.4 to version 4.4.138
    cf4a37a581 uci: add missing 'option' support to uci_rename()
    7fc94b2a25 mac80211: rt2x00: no longer use TXOP_BACKOFF for probe frames
    b03826d8aa kernel: bump kernel 4.4 to version 4.4.137

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=21f44e338976f54812a8f917e6648d8167a26936
+OPENWRT_COMMIT=38e704be71e5676a8b0bed5bbf74a2784acdaa01
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

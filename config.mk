514acf20435a4c182241d1bfba4049c13b705438
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-09-05 21:54:45 +0200

    update OpenWrt and feeds to lede-17.01.6
    Releasenotes: https://openwrt.org/releases/17.01/notes-17.01.6
    Changelog: https://openwrt.org/releases/17.01/changelog-17.01.6

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=788312ca59c57f1cfc4737378526ff1c2a1c8374
+OPENWRT_COMMIT=184fe11483b9595f31a028356f8cc18fb18536e3
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

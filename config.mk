ad5c3deaaa2e998189378d48f3233c837790391e
pmelange <isprotejesvalkata@gmail.com>
2018-12-10 13:41:16 +0100

    build: add tunneldigger images

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin backbone
+PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=202c3ceb13032b9fcb6eb2b87286a0ff7197a5de
 SET_BUILDBOT=env

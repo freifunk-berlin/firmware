4e1f5116d54de4435c09cc9f840ed0373c3250a0
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-20 22:41:16 +0100

    update OpenWRT to HEAD of LEDE-17.01 (19. Jan 2019)
    e09592748c cns3xxx: use actual size reads for PCIe
    9d4eed6837 mac80211: brcmfmac: fix use-after-free & possible NULL pointer dereference
    834bd86424 apm821xx: switch MX60(W)'s recovery images to multi-image method
    459a40fce2 apm821xx: MBL: load kernel/dtb from SATA 0:1 first

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=202c3ceb13032b9fcb6eb2b87286a0ff7197a5de
+OPENWRT_COMMIT=e09592748c5c02ee34b75508e9d72e50a1e0920a
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

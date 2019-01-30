2823ec3c3ae2563ac96df3b538808626bfc636b7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-01-31 00:16:00 +0100

    update to OpenWrt and feeds to v18.06.2 release
    see https://openwrt.org/releases/18.06/notes-18.06.2

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=753531da249f4960b1088513665982322a2b6a97
+OPENWRT_COMMIT=a02809f61bf9fda0387d37bd05d0bcfe8397e25d
 SET_BUILDBOT=env
 MAKE_ARGS=

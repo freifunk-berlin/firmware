b39253525aa4056b186424076f0de2592472bfc0
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-02-02 14:25:05 +0100

    Update OpenWrt to HEAD of lede-17.01 branch
    484117b478 base-files: fix postinstall uci-defaults removal
    0c8e8e2dc9 base-files: install missing /etc/iproute2/ematch_map
    99e5ba3207 opkg: drop argument from check_signature in opkg.conf
    f0115b5011 opkg: switch source url to git.openwrt.org
    009ecf3149 opkg: update to latest Git head
    6c14b29bf1 openssl: bump to 1.0.2q

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=e09592748c5c02ee34b75508e9d72e50a1e0920a
+OPENWRT_COMMIT=6c14b29bf1c28ca2cb171b1a46d1c880c0794f1d
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

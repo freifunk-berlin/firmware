22a6833130b0632686dcae673b01ad21a3b8125a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-03 00:10:15 +0100

    Update to HEAD of OpenWrt-master
    858aebc0ef oxnas: unify MTD partition names
    9a9c6f37d5 uboot-envtools: oxnas: sync with current oxnas/ox820 DTS
    5fac36d5aa oxnas: switch to kmod-ledtrig-usb-port
    266b3da335 oxnas: remove uboot-oxnas-ox820 from default package selection
    7cf6e11721 mac80211: rt2x00: patch tracking cosmetics
    98f86e61ea mac80211: rt2x00: cleanup ePA, RXIQ and TX-LOFT code
    b462bab099 ath79: Fix indent
    157072ea2b busybox: unindent busybox Config.in
    eb6f5a58b9 busybox: sync Config.in files
    0e8ddc953f libubox: bump to version 2019-02-27
    82d306b595 mac80211: backport tx queue start/stop fix
    c6caa7a27a mac80211: add a fix to prevent unsafe queue wake calls during restart
    e2fea41ad6 oxnas: fix forgotten MTD partition name
    e882d5bf31 iwinfo: update to latest git
    6b2874707a oxnas: switch to DTS aliases for LEDs and use generic diag.sh
    4c70ac7be4 x86: fix geode image builds redux
    21e90685b6 ath79: speed up ath9k-eeprom extraction
    ffd8ca18cc apm821xx: speed up ath9k-caldata extraction
    cb911a05b0 layerscape: fix VFIO driver build issue since linux-4.14.99
    6c558bae64 ath79: TP-Link Archer C7 v4 swap usb led names
    f2c83532f9 ramips: add support for Belkin F9K1109v1
    6837c757cf ath79: make TP-Link revision naming consistent
    26cb167a5c mpc85xx: Fix Aerohive HiveAP-330 initramfs image
    42f2e07ba0 oxnas: cloudengines,pogoplug*: enable USB ports
    bc97257ffe ltq-vdsl-fw: update download URL

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b2bf3745ff7e5e2fbf3b7b0e488cfaa5b3cca87c
+OPENWRT_COMMIT=bc97257ffefd560d7e77fec8c6ac9d3745ea9f11
 SET_BUILDBOT=env
 MAKE_ARGS=

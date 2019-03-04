297d93559bacf17a5e6e373d3e5ae5f04dc319c5
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-05 00:10:14 +0100

    Update to HEAD of OpenWrt-master
    c1a8054114 oxnas: add SoC restart driver for reboot
    4590af2065 mac80211: Activate DRIVER_11W_SUPPORT for more capable drivers
    33201a3ad1 mt76: update to the latest version
    ac3bae907f tools/squashfskit: fix version detection on non-linux system
    068c4cf407 tools: squashfskit4 should only depend on coreutils on non-linux systems
    dcb08049ae oxnas: cleanup and improve pcie driver
    302f7d57a9 oxnas: restart: use register definitions from dt-bindings
    3bb9dcf446 oxnas: convert pcie to builtin_platform_driver
    e7aa4c0db7 oxnas: pcie: model shared resource as external pcie-phy driver

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=bc97257ffefd560d7e77fec8c6ac9d3745ea9f11
+OPENWRT_COMMIT=e7aa4c0db7eb3e81163682f570a9aecbb9fa8aed
 SET_BUILDBOT=env
 MAKE_ARGS=

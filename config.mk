89198454a3e77010fd29d65e2c3cc8f2020c772b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-06 00:10:16 +0100

    Update to HEAD of OpenWrt-master
    f73b2d64ed ath79: ag71xx: replace ag71xx_get_phy_if_mode_name() with phy_modes()
    a4eef43a12 ath79: ag71xx: replace alloc_etherdev with devm_alloc_etherdev
    f92bbdcc93 ath79: ag71xx-mdio: get reset control using devm api
    596a5325cd ath79: ag71xx: register mdio using of_platform_populate
    cea3e9e438 ath79: ag71xx: add support for optional mdio reset
    51cdf0e9eb ath79: ar934x: assert mac/mdio reset together for gmac0
    47f0be676f ath79: qca955x: assert mdio/gmac reset together
    368b6d1a52 ath79: gmac: ar934x: Add parser for mii-gmac0-slave
    40530c8eb4 ath79: Add support for Ubiquiti Nanostation M (XW)
    d0b969eee8 mac80211: rt2x00: do not increment sequence number while re-transmitting
    fc45ae4461 kernel: bump 4.9 to 4.9.161
    1cfbf95393 kernel: bump 4.14 to 4.14.104
    d3506d1bc1 ar71xx: ag71xx: fix compile error when enabling debug
    64bb88841f uqmi: inherit firewall zone membership to virtual sub interfaces
    9e32e288f0 kernel: fix refcnt leak in LED netdev trigger on interface rename

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=e7aa4c0db7eb3e81163682f570a9aecbb9fa8aed
+OPENWRT_COMMIT=9e32e288f0de3fa8c7ae1d193aec608648430759
 SET_BUILDBOT=env
 MAKE_ARGS=

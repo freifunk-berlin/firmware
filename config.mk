2a9438321c5c8aca897301677b6ce71fa86f5137
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-07 00:10:13 +0100

    Update to HEAD of OpenWrt-master
    26c16aec67 metadata: fixup "board"
    208f287908 metadata: add "target" entry
    7fa9794c4c metadata: add "metadata_version" field
    c8778250a9 ramips: mt7620: export chip version and pkg
    13c379e5c6 ib: display whether profile comes with image metadata
    33727ecea5 lantiq: enlarge ZyXEL P-2812HNU-F1 kernel partition (FS#2124)
    5c83f27ac5 ath10k-ct: Update to 2019-02-28
    fc2fd1c9d6 ath10k-firmware: update Candela Tech firmware images
    4484ef587a ath10k-ct: limit available channels via DT
    fe591f2fe2 ar71xx: enable QCA955x SGMII fixup on Rambutan
    e15bda603a ramips: rework network settings for HC5861
    a4ba41863b mips: fix dynamic ftrace
    457de5a154 owl-loader: fix crash caused by endian patch

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9e32e288f0de3fa8c7ae1d193aec608648430759
+OPENWRT_COMMIT=457de5a15412bae6d7a590e7316a3996de318b37
 SET_BUILDBOT=env
 MAKE_ARGS=

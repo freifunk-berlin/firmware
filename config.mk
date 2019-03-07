cd48e5717d9fbf258545c334461874e985568412
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-08 00:10:14 +0100

    Update to HEAD of OpenWrt-master
    fd8705e635 tools/mkimage: use http download server (fixes FS#2052)
    3ca38dcfa2 kernel: bump 4.9 to 4.9.162
    364ab348dc kernel: bump 4.14 to 4.14.105
    9f818cb002 mt76: update to the latest version

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=457de5a15412bae6d7a590e7316a3996de318b37
+OPENWRT_COMMIT=9f818cb002d210e4bc8355c1ae1d1edd94630e4a
 SET_BUILDBOT=env
 MAKE_ARGS=

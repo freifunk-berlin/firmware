8601d378b01ca037fbbd4e5db3e529df164f15af
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-10 00:10:17 +0100

    Update to HEAD of OpenWrt-master
    69d3c7e6da ucert: add PROVIDES also for minimal 'ucert' package
    f37afec866 ib: show unified target based on $(TARGETID)
    483498808e ib: show current revision based on $(REVISION)
    6ee73942f9 ipq-wifi: update ALFA Network AP120C-AC board-2.bin
    ff8a8074b2 ipq40xx: add ath10k-calibration-variant in AP120C-AC DTS
    24de7c29e5 ipq40xx: backport I2C QUP driver changes from 4.17
    6fa94d5e8f ath79: add support for YunCore A770
    c5394ec7e0 uboot-envtools: ath79: add support for YunCore A770
    c3425be082 6to4: update OpenWrt documentation URL
    d971ae51a5 openssl: backport devcrypto changes from master
    445ca981d1 sunxi: add support for Orange Pi PC Plus

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9f818cb002d210e4bc8355c1ae1d1edd94630e4a
+OPENWRT_COMMIT=445ca981d163d54c8dcf16cf206bc44904160b99
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

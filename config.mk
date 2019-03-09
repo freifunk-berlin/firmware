1888f29d7a68c1801a10187507c1b3c79302f12f
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-09 01:25:48 +0100

    Update OpenWrt to HEAD of lede-17.01 branch (8. March 2019)
    f3f21b3d2a mac80211: brcmfmac: fix a possible NULL pointer dereference
    fb333a7fec mac80211: brcmfmac: pick few 4.17 cleanups required for further fixes
    eab6537449 mac80211: brcmfmac: backport 5.0 & 5.1 important changes/fixes
    981f5f7e40 kernel: fix refcnt leak in LED netdev trigger on interface rename

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default vpn03 tunnel-berlin tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6c14b29bf1c28ca2cb171b1a46d1c880c0794f1d
+OPENWRT_COMMIT=981f5f7e40cbf26bc0beb0a2aa5f3c562b83c85b
 SET_BUILDBOT=env
 MAKE_ARGS=
 #BUILDTYPE - unstable / release

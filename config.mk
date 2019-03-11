a2083952bcc58473417ac3f3002698aa56d642f1
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-12 00:10:15 +0100

    Update to HEAD of OpenWrt-master
    50717510e7 fritz-tools: add fritz_tffs_nand_read tool
    20f48c8ae3 lantiq: add support for AVM FRITZ!Box 7412
    5026f43559 lantiq: move common FRITZ736X bits into dtsi
    eae6cac6a3 lantiq: add support for AVM FRITZ!Box 7362 SL
    ff3cfe0848 ltq-atm/ltq-ptm: re-enable/fix reset_ppe() functionality for VR9
    7fc4dd3611 cmake: match warnings more strictly in C++ feature checks
    3d3e04d8c8 wolfssl: fix build in busybox environments
    17468cc090 layerscape: rename Traverse boards along SoC lines
    6a2705a52f layerscape: use wrapped nand_do_upgrade for traverse-ls1043 boards
    8f9155fe7a oxnas: fix PCIe register ranges in device-tree
    26af8e48d3 linux-atm: Fix compile problem with kernel 4.20
    a18e2994df lantiq: Refresh kernel 4.14 configuration
    b04c9a1ffc nghttp2: bump to 1.37.0
    a18d41996e tools: tar: update to version 1.32
    8ad0ba3a07 oxnas: move PCIe controller outside of simple-bus
    35a7f79096 oxnas: use generic filenames for image and DTS
    d6fa04a437 IB: include SUPPORTED_DEVICES in 'make info' output
    87a81455a1 oxnas: replace oxnas-restart with version to be submitted to upstream
    b4917fa907 oxnas: fix oxnas-rps-timer dt-match
    2cd28c9a08 busybox: add missing install dir
    6ac5144b13 mxs: disable unused serial drivers
    9b6413982c ramips: add support for ALFA Network Tube-E4G
    82dbcd8bf2 uboot-envtools: ramips: add support for ALFA Network Tube-E4G
    db91418766 ath79: add support for TP-Link RE350K v1
    5107176861 procd: Enable seccomp for powerpc

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=445ca981d163d54c8dcf16cf206bc44904160b99
+OPENWRT_COMMIT=510717686189f4a6341635606fb7858217d627d3
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

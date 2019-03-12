78d7cfacfbcf9de43c6edb87c8d35c30ea6648ef
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-13 00:10:14 +0100

    Update to HEAD of OpenWrt-master
    0e9b11213e oxnas: enable pcie_phy on boards utilizing PCIe
    74ef848b96 oxnas: also akitio,mycloud uses PCIe apparently
    36530ba72a mac80211: rt2x00: import and update pending patches
    8c593804d0 oxnas: install wireless driver on pogoplugpro
    2407b1edcc openssl: disable digests by default, misc fixes

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=510717686189f4a6341635606fb7858217d627d3
+OPENWRT_COMMIT=2407b1edccc2f2d426333bd7cc1743c8e4da8dbd
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

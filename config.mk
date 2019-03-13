2e5579de10a9deaea526ae73f727982657460727
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-14 00:10:16 +0100

    Update to HEAD of OpenWrt-master
    39092ad9e9 ipq40xx: tidy up 02_networks
    784f2e73df ipq40xx: fix phy interrupt setting
    93601d647f ipq40xx: fix FRITZBox 7530 NAND controller node
    24463d0d94 ath79: add support for LibreRouter v1
    d009033911 ramips: add support for Xiaomi Mi Router 3 Pro
    7dfe357a3d ramips: HC5861 drop unused gpio group define in dts
    fefa34def8 ath79: Add GL.iNet AR-300M-Lite
    d75db67870 uboot-fritz4040: bump version to 2019-03-03
    5389db72e3 fritz-tools: add support for IPQ40xx platform
    148d29d47b ipq40xx: add support for AVM FRITZ!Repeater 3000
    41a86debe3 ipq40xx: limit frequencies for Asus Lyra
    ee34740ca3 ath79: add suport for EnGenius EPG5000
    b907097291 kernel: mtdsplit: wrgg: Support big and little endian
    ac5f431ad1 gemini: Generate padded kernel+rootfs images for DIR-685
    91a7791115 apm821xx: usb: xhci: fix semicolon.cocci warnings
    e1ba59ea5c ath79: Speed up caldata/eeprom handling
    51fb186cb3 ar71xx: Speed up caldata/eeprom handling
    26f7cf8ac3 ar71xx: Speed up mtd extraction in ar71xx.sh
    96e0fa94c7 ath79: ag71xx: Remove ndo_poll_controller
    e991fb44e0 uboot-sunxi: bump to 2019.01
    5e247f3ac1 uboot-sunxi: add fix for A20-OLinuXino-Lime2-eMMC rev. K boards

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2407b1edccc2f2d426333bd7cc1743c8e4da8dbd
+OPENWRT_COMMIT=5e247f3ac118c0675d9e9f29a0b1fb8a959b5155
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

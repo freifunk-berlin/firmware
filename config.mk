dc9416601cedf057c420b3b930df7e3f611285cb
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-17 00:10:16 +0100

    Update to HEAD of OpenWrt-master
    51e1092fae mac80211: allocate tailroom for forwarded mesh packets
    5cda4a3e30 mac80211: update and fix the patch to allow 4-byte aligned tx skbs
    fd8ca8deb3 netifd: add support for suppressing the DHCP request hostname by setting it to *
    fb0a80f4cf mac80211: fix an unaligned access in the mesh hash table function
    04e4b779cc mac80211: backport the txq scheduling / airtime fairness API
    6869ae2ab5 mac80211: improve locking around the txq scheduling / airtime fairness API
    1f68aac9d7 kernel: backport flow dissector batman-adv support
    4cf1394f51 mac80211: add a few performance improvement patches
    8852a09d85 mt76: update to the latest version
    dd3214f95d mac80211: netifd: Use a mask when using `iw set antenna`
    a3446257a8 tools/cmake: Update to 3.14.0

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b61495409bb398b1967d7da305a49a6f077cb55a
+OPENWRT_COMMIT=a3446257a8737bfc2899c5911791873561feecc7
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

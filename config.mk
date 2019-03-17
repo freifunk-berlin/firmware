bd6dc16b9a8239f98bf7f0d00cfaf91fb96eb10a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-18 00:10:15 +0100

    Update to HEAD of OpenWrt-master
    a56c21f582 tools/libelf: Add mirrors as main site is dead
    d454035430 Revert "tools/cmake: Update to 3.14.0"
    89c8232f78 ipset: size optimizations

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a3446257a8737bfc2899c5911791873561feecc7
+OPENWRT_COMMIT=89c8232f78f4acd982f66bd285f1448bbbad1719
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

50c72909a9c671b398f9c622cdd16017b17e593d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-21 00:10:15 +0100

    Update to HEAD of OpenWrt-master
    79fc997e48 kernel: bump 4.9 to 4.9.163
    a8cfef6c45 kernel: bump 4.14 to 4.14.106
    50674ccade ath79: fix leds description for EnGenius EPG5000
    9fd2a3faa5 ath79: qca955x: add wmac migration hotplug event
    f3da2ebf90 lantiq: kernel 4.14: fix usb_phy1 reset status bit in vr9.dts
    8f6f260302 ath79: routerstation: prepare to use sysupgrade-tar format image
    b3a0c97b8c ath79: add support for jjPlus JA76PF2
    9b16a69941 usbutils: Update usb.ids to 0.321
    c3a17ee4b3 ramips: add #pwm-cells property to MT76x8 dts
    f1396ac753 ath79: align naming of Ubiquiti Nanostation M
    57b1e951b5 ipq806x: Enlarge R7500v2 flash - use netgear partition
    e7a7749a3c kernel: fix compile issue for tda1997x media device
    6d5762f6d8 kmod-sched-cake: bump to latest cake
    ffb2a3aa2a iproute2: add cake fwmark support
    60659b2d9e mac80211: optimize skb resizing to avoid reallocation when using 802.11s + batman-adv
    ca9ad880f2 mac80211: improve the txq scheduling API to deal with driver buffered packets
    38860fe2e4 mac80211: fix an issue with the TXQ scheduling API and powersave clients
    6c7b49c8b9 kernel: Update arc-add-OWRTDTB-section patch for 4.14
    343b36ffc7 toolchain: Use GCC 8.3.0 by default for ARC
    075c76ea67 toolchain: Remove GCC 7.1 for ARC targets
    da6ba853ce toolchain: Use binutils 2.32 by default for ARC
    8568dcd931 toolchain: Remove binutils 2.29 for ARC targets
    31078dbc76 iproute2: update to 5.0.0
    846f98056a kernel: Backport ar933x_uart build fix (fixes FS#2152)
    d2c3eb1e43 kernel: brcm47xx: Backport fixes for USB on WNDR3400v2 (fixes FS#2170)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=89c8232f78f4acd982f66bd285f1448bbbad1719
+OPENWRT_COMMIT=d2c3eb1e431fa8b1421c3633576781e5d4eede58
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

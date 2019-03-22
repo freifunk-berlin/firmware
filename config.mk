0de2017a6e59913ec0a27a3e22e0f512daf446f3
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-23 00:10:15 +0100

    Update to HEAD of OpenWrt-master
    5c11258297 ramips: add support for I-O DATA WN-AC733GR3
    dc4f6b896f ipq40xx: add support for EnGenius ENS620EXT
    f22c33b40c ca-certificates: update to version 20190110
    201d3d1a82 ramips: Xiaomi MIR3G: detect board name from DTS
    4269914187 ramips: Speed up eeprom read/write
    133bad46a6 adb: fix missing PKG_MIRROR_HASH
    41bceb0d4e uboot-fritz4040: fix PKG_MIRROR_HASH
    78b9d57882 ath79: GL.iNet AR300M-nand/nor: correct LAN LED
    34113999ef ar71xx: Remove ath10k packages from archer-c7-v1 (fixes FS#1743)
    1ca69003fd odhcpd: update to latest git HEAD (FS#2160)
    fc1386ccf8 openssl: revert disallowing parallel build
    488e7ccfbc libnftnl: bump to latest version
    b36d250b55 Revert "valgrind: enable LTO and refresh patches"
    9725aa271a iwinfo: update to latest git HEAD
    0168af502b ubox: update to latest git HEAD
    94ffb7be4d netifd: update to latest git HEAD
    9be901061e build: suppress error output in feeds.mk
    5ac974f214 ar71xx: Add support for Mikrotik RB SXT 2nD r3
    d78e229903 ath79: Add support for Ubiquiti NanoBeam AC
    076c11a3aa glibc: update to latest 2.27 commit
    d24bac0a95 imx6: refresh kernel configs
    74f3cc04cd imx6: enable crypto acceleration
    8877fdbf02 imx6: enable crypto acceleration (4.19)
    f7a057871b imx6: enable PCIe (4.19)
    9c63141025 imx6: fix legacy IRQ support (4.19)
    fa91a147bb kernel: bump 4.9 to 4.9.164
    0304e76cd1 kernel: bump 4.14 to 4.14.107

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d2c3eb1e431fa8b1421c3633576781e5d4eede58
+OPENWRT_COMMIT=0304e76cd1124dd4ff70ad1a540b5719cc991b43
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

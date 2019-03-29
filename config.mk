3adcef7f2e5604213e1f0ea6ef3feb83ae99896a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-30 00:10:14 +0100

    Update to HEAD of OpenWrt-master
    3d945f5706 mvebu: Refresh kernel configuration
    63ed513779 mvebu: Add dependency to kmod-i2c-mux-pca954x for armada-macchiatobin
    e1444ab59c ar71xx: add support for MikroTik RouterBOARD 922UAGS-5HPacD
    b422e1631e scripts/gen_image_generic.sh: fail on errors
    4bc0224149 kernel: bump 4.9 to 4.9.165
    af6c86dbe5 kernel: bump 4.14 to 4.14.108
    b65a270c85 mt76: update to the latest version
    555ee02f77 kernel: fix missing dependency in 4.14.108
    b2152c8e6b odhcpd: update to latest git HEAD (FS#2204)
    1e8bb50b93 wireguard: do not add host-dependencies if fwmark is set
    7204736076 IB: fix generating .profile.mk for profiles without metadata
    28920330f8 wireguard: introduce 'nohostroute' option
    95f07502b7 package/uboot-omap: backport patches to fix build
    1e55171a12 fstools: update to the latest master branch
    b0395cfc56 iwinfo: Fix 802.11ad channel to frequency
    7c4b85ee27 ramips: add support for WIZnet WizFi630S board
    58becdb30d ramips: add support for ZyXEL Keenetic Start
    3f019bf96c ramips: add Netgear EX6150
    274439b780 ramips: Increase GB-PC2 SPI frequency to 80MHz
    2159b25466 ramips: Increase GB-PC1 SPI frequency to 80MHz
    8305c475cc kernel: bump 4.9 to 4.9.166
    968baed351 kernel: bump 4.14 to 4.14.109
    6df5ab89cf odhcpd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ae9d3a25c2cb165912904dc8b57dbc5da24c61e1
+OPENWRT_COMMIT=6df5ab89cff8c2b2cd1a24a3e365c11b472497db
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

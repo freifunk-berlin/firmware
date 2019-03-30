e86c40169dcbf6177cca8f20ee539b1616d807ea
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-03-31 00:10:17 +0100

    Update to HEAD of OpenWrt-master
    fbe2e7d15e ath10k-firmware: update Candela Tech firmware images

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6df5ab89cff8c2b2cd1a24a3e365c11b472497db
+OPENWRT_COMMIT=fbe2e7d15e5bf6c4ae2a2e4189f9b389098c3245
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

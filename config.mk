196dd63f3ac7238bea3fa9894ba13f97d1d0356a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-02 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    d260813d09 ar71xx: ens202ext: Fix VLAN switch
    38f3433420 ramips: add missing SPDX identifier for EX6150
    6ba3d70c95 mwlwifi: Fix pcie timeout issue
    4376c06e80 openvpn: update to 2.4.7
    0a67e84bae ar71xx: ens202ext: Fix whitespace issues
    6027e49093 build: image: Fix off-by-one in DTC kernel version checks
    848d85d13b netifd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=fbe2e7d15e5bf6c4ae2a2e4189f9b389098c3245
+OPENWRT_COMMIT=848d85d13b6602ff480d3de0339ebef9f79d56ff
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

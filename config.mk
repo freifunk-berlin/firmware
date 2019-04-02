4c6867cdc959823ca4b73cfb743e0b898db958ce
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-03 00:10:14 +0200

    Update to HEAD of OpenWrt-master
    a2a972b2cf ar71xx: Use dynamic partitions for TP-Link CPE210 v2
    99f6f2c0b8 ar71xx: Add support for TP-Link CPE210 v3
    5ec205d7ac ath79: Add support for TP-Link CPE210 v3
    141698ce8f ath79: Add support for TP-Link CPE210 v2

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=848d85d13b6602ff480d3de0339ebef9f79d56ff
+OPENWRT_COMMIT=141698ce8fbc52d253c9207ef46b3630a0870567
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

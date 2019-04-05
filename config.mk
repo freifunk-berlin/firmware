373154482f660e839846f2bec9612643df1c6dad
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-06 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    9a92af4624 kernel: fix rtcache compilation with 4.18+ with IPv6 support
    ebed4b6dd0 kernel: backport upstream ubifs default compression selection fix
    8dacd75ea8 kernel: b53: add support for kernels 5.0+
    6e060bd62c base-files/hotplug: fix dedicated group for tty devices
    f483274422 odhcpd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b3d8b3ab8e6fc0c0355f0680fc1c5f9c90a0c35a
+OPENWRT_COMMIT=f483274422acbe0219c48008e18926abf2c280de
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

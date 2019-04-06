c42fa199b643429628cf0b3dd308d9b5bfab7a80
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-07 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    de3eb0d8a0 curl: Update to 7.64.1
    42f96ed941 tegra: add new target
    8595bb0b17 tools: add cbootimage for tegra
    b874437644 tools: add cbootimage-configs for tegra
    1b3dda179a uboot-tegra: add U-Boot for tegra boards
    6541897796 kernel: package rtc-em3027 module
    bba0c012b8 tegra: add support for CompuLab TrimSlice
    09fe18f042 tegra: add kernel 4.19 support
    4ebd66d7a9 mbedtls: update to version 2.16.1
    3183430df4 mac80211: update to version 4.19.32-1
    5a739bd43e zynq: kernel: refresh config
    82b0230bc1 kernel: sound: add missing symbol to sound-soc-core
    95c531dc07 zynq: kernel: remove sound kconfig options
    6e303b4532 zynq: kernel: remove video/camera kconfig options
    177a634e18 kernel: can: add Xilinx CAN IP kernel module package
    3db12da7cd zynq: replace CAN kconfig options by device packages
    78a3c51985 zynq: replace storage related configs by packages
    910eb994eb mvebu: make bootfs size for sdcard image configurable
    55b4ff7f73 mvebu: use ext4 for clearfog image bootfs
    727c3df5bb mvebu: shrink amount of packages and reorganize them
    4e8345ff68 mvebu: base-files: autodetect upgrade device
    2e5a0b81ec mvebu: sysupgrade: sdcard: keep user added partitons
    6c22545225 target/octeon: Add Linux 4.19 support
    549d44736a wireguard: bump to 0.0.20190406
    32689e93c4 ramips: add support for Youku YK-L2
    afef17e24d base-files: add leds migration
    bac2c4d2fb ath79: engenius epg5000: add leds migration script
    5198258d10 tools: tplink-safeloader: add C7v5 KR Support
    b19c48dc34 ramips: enable R6120 USB power
    2a8175a7ac kernel: Add RIPEMD160 module
    0eef05fb36 ath79: Utilize tplink-safeloader definition
    46dc4e206d tools/xz: Compile with PIC to fix linking errors
    007e947976 fconfig: cleanup Makefile
    32a6c252db wpan-tools: clean up Makefile
    8252511dc0 build: remove sgid permission from tar
    d80a1c6e2c ramips: backport mt7621-spi from linux-next-20190329
    fac27643f0 firmware-utils: add nec-enc
    643fcfc8d6 ath79: add support for NEC Aterm WG1200CR
    b812a7fa68 ramips: correct DTS for Belkin F9K1109v1 to include switch definition
    7f3facfce2 firmware-utils: fix nec-enc build on older architectures

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=f483274422acbe0219c48008e18926abf2c280de
+OPENWRT_COMMIT=7f3facfce29639ec50677aa2b0eecebe259aa4d3
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

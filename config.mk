d9f10b09970ad702bfd8ba20f8eb3e204e57c593
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-08 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    f0be038897 tools: cbootimage: depend on automake
    310e2764a8 ubox: bump to git HEAD
    80568e5854 dropbear: bump to 2019.78

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=7f3facfce29639ec50677aa2b0eecebe259aa4d3
+OPENWRT_COMMIT=80568e58545fc2c3622022bbbc77ea36987fefa5
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

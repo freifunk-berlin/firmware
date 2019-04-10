322af99a7ff0bd49e28e881e930f2327fc643e21
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-11 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    d1739c6c9a procd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2101002b3d21d1c4a03c5333e02d952956b7c394
+OPENWRT_COMMIT=d1739c6c9a3618bafc04388b0c0209951125897a
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

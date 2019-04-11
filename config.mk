fedf04db42007eb070abcd5b367f04bad0370123
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-12 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    af606d077f hostapd: fix CVE-2019-9494
    262229e924 hostapd: fix CVE-2019-9495
    57ab9e3add hostapd: fix CVE-2019-9496
    8f17c019a1 hostapd: fix CVE-2019-9497, CVE-2019-9498, CVE-2019-9499
    750a57b836 build: switch default target from ar71xx to ath79
    44ae5f37fb uboot-envtools: fix fw_env.config for ox820/stg-212
    b6acb7b718 kernel: move and replace accepted patch

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d1739c6c9a3618bafc04388b0c0209951125897a
+OPENWRT_COMMIT=b6acb7b718e3bf47f8dfa615345120aad0930bad
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

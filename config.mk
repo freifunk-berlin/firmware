6b432844b38b8dbceb56a0a0fb0d5f1bcbb71ba4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-13 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    66e2acad9c ramips: fix pinctrl to allow hardware i2c on WRTNODE2R
    9385ff654e mac80211: rt2x00: replace patch with upstream version

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b6acb7b718e3bf47f8dfa615345120aad0930bad
+OPENWRT_COMMIT=9385ff654e4d9ef1270b2cbb411be3754513c9ed
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

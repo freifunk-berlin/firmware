078cb423d6d7f749ae335f2e0acdde5cbd7657c9
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-15 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    e0d98e120a ath79: add TP-Link WR842N v3
    3fd443640c ath79: remove unnecessary packages from I-O DATA ETG3-R
    68b8d3b079 kernel: usb: add FSL EHCI package
    84b1257f80 mpc85xx: remove USB support from kernel
    bb831ca43a mpc85xx: clean up device package selection
    b3f7a73860 mpc85xx: generic: Check kernel size for the TL-WDR4900
    43e8c37cb4 ramips: add support for GL.iNet VIXMINI
    9e0fb5a27f ramips: add support for Rakwireless RAK633
    8293e7532f mac80211: Fix rate_idx underflow in mwl8k (FS#2218)
    5c8f557b36 Revert "ath79: Add WPS button to TP-Link Archer C7v5"

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9385ff654e4d9ef1270b2cbb411be3754513c9ed
+OPENWRT_COMMIT=5c8f557b363ac6b235e00804cad02450793a49a8
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

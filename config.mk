df0e9065c2db70c635fb29b2e1224c199f8c6601
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-16 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    ecdd26fe2b umbim: update to latest git HEAD
    78ee6b1a40 kernel: MIPS: perf: ath79: Fix perfcount IRQ assignment
    324e94f31b mpc85xx: disable bricking TL-WDR4900v1 images
    d599890efd layerscape: unbreak ehci-fsl interaction with mpc85xx
    3e803499c3 netifd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5c8f557b363ac6b235e00804cad02450793a49a8
+OPENWRT_COMMIT=3e803499c3661e4df3639dd797b46cf0719753c3
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

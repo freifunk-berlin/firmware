7d37fec0a2946bca583cfee0409e6999dc04065b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-18 00:10:14 +0200

    Update to HEAD of OpenWrt-master
    e79b9601bf procd/hotplug: add dependency to dialout and audio group
    e762f5d44a kernel: Fix kmod-drm-amdgpu and kmod-drm-radeon dependencies
    450d44a8ea openssl: change defaults: ENGINE:on, NPN:off, misc
    e20c2909a5 odhcpd: update to latest git HEAD (FS#2206)
    bd3a18bbe4 lantiq: tdw90x0: Fix WLAN LED on TP-Link W8970 v1.2 (FS#2232)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=3e803499c3661e4df3639dd797b46cf0719753c3
+OPENWRT_COMMIT=bd3a18bbe433cc53b6f86dd708477f97ac406abc
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

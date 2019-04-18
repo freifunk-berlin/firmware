0d46fc18c5e3ee2f31442886cb2c271dacbc4ea4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-19 00:10:18 +0200

    Update to HEAD of OpenWrt-master
    083056c83f mac80211: brcm: backport brcmfmac 5.2 patches
    5a829af69a include: unpack.mk: remove CRLF_WORKAROUND
    16b381179f kernel: add missing drm symbol
    ee38ffb4a9 armvirt64: enable qemu drm drivers

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=bd3a18bbe433cc53b6f86dd708477f97ac406abc
+OPENWRT_COMMIT=ee38ffb4a9a95eb350b9791fda8439521507c7e2
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

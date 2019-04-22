d7671d0e725b6393bec6d5516b2d3f28f3dddcdc
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-23 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    524810ce6d dropbear: allow build without dbclient
    399aa0b933 odhcpd: update to latest git HEAD (FS#2243, FS#2244)
    13ffdf4482 toolchain/binutils: refresh patches
    47dc4f96cb nghttp2: bump to 1.38.0
    ff9ac986ce openssl: fix OPENSSL_config bug affecting wget
    6e243c2353 kernel: bump 4.9 to 4.9.169
    da5bd73d70 kernel: bump 4.14 to 4.14.112
    022dfdc41c kernel: bump 4.19 to 4.19.35
    8abb505048 openssl: add Eneas U de Queiroz as maintainer

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ee38ffb4a9a95eb350b9791fda8439521507c7e2
+OPENWRT_COMMIT=8abb505048bd827ed8774bd94a439d75deb43d97
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

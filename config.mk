c980a552a012e1cf2a56fd695799b8e9b17d08a8
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-24 00:14:14 +0200

    Update to HEAD of OpenWrt-master
    f00a4ae6e0 Revert "uhttpd: disable concurrent requests by default"
    d27c245216 kernel: bump 4.9 to 4.9.170
    07aaa7e3d6 kernel: bump 4.14 to 4.14.113
    f337cd2ba3 kernel: bump 4.19 to 4.19.36
    6afe175e5e ath10k-ct: Update to 2019-04-08
    e40050d104 ar71xx: add support for Mikrotik RouterBOARD 2011UiAS-2HnD r2
    82625374ee toolchain/musl: update to version 1.1.22

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=8abb505048bd827ed8774bd94a439d75deb43d97
+OPENWRT_COMMIT=82625374eea0613027d8dbe2bcfdce7beadaa883
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

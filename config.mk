92e9cf09049a1c19f7ed302b8b060dc186eddc04
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-04-28 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    40de4c038a elfutils: bump to 0.176
    a9f6fceb42 sdk: fix building external modules when CONFIG_STACK_VALIDATION=y
    647356a3d2 scripts/qemustart: no network by default and enable with -n
    d726878546 scripts/qemustart: fix network device emulation
    c2fee615ce scripts/qemustart: add notes on adding new options
    17cb490ac4 openssl: build kmods only if engines are selected
    9af2735734 kernel: fix regression on 4.19 with 613-netfilter_optional_tcp_window_check.patch (FS#2253)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9424b6f998917e2926c0b7afb8d7a968590da335
+OPENWRT_COMMIT=9af2735734fce61eaf9bdb49fff218e6548af6f2
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

1afe70ce5ea7eb57ccbfa805747e38cde1c1a471
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-06 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    ed5409dbe9 tools/ccache: update to 3.7
    fdf1aae77d kernel: add missing symbol in 4.19 config
    6b5346e7ed glibc: update to latest 2.27 commit (BZ #18035)
    26dafeeba4 mac80211: rt2x00: replace patches with upstream version
    430b66bbe8 procd: update to latest git HEAD
    d6643aca34 libroxml: bump to the 3.0.1 version
    a9190ee3a4 kernel: iio: Fix BMP280 Auto probing
    456f486b53 kernel: bump 4.9 to 4.9.171
    b765f4be40 kernel: bump 4.14 to 4.14.114
    08802d93e2 kernel: bump 4.19 to 4.19.37
    a12ab07e21 procd: allow passing optional syslog facility as instance parameter
    218b1bbecd procd: allow passing optional group instance parameter
    8696f0c3e3 procd: update to latest git HEAD
    2c3dd70741 procd: add procd_running() helper for checking running state
    49dca520fb brcm2708: fix renamed definition (FS#2265)
    2d0a2ff1e0 adm5120: Remove unmaintained target
    c35425f418 adm8668: Remove unmaintained target
    e6f9a8e89b au1000: Remove unmaintained target
    65cdfbabde mcs814x: Remove unmaintained target
    406a3826dc ppc40x: Remove unmaintained target
    8ba38cc145 ppc44x: Remove unmaintained target
    cd3b298533 omap24xx: Remove unmaintained target
    675832de79 xburst: Remove unmaintained target
    1325e74e0c kernel: Remove support for kernel 3.18
    97d3f800a8 config: kernel: Add KPROBE_EVENTS config option
    51216b4f83 kernel: Rename UPROBE_EVENT to UPROBE_EVENTS
    3b4c44aa8b ar71xx: mute script error in ar922x-led-fix script
    fc23bcdaa2 base-files: add service_stopped as a post stop hook
    3364af629d ath79: fix OCEDO Raccoon 10Mbit PLL value
    edf1d6264b ar71xx: fix failsafe interface for TL-WR940nv6
    2437418cd5 ath79: glinet_gl-ar750s: Use QCA9887 firmware
    b18d1d5d3f uboot-tegra: bump to 2019.04
    4760541027 elfutils: Fix compile with uClibc-ng

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9af2735734fce61eaf9bdb49fff218e6548af6f2
+OPENWRT_COMMIT=476054102791d395af7225e47e6024573bebf2fe
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

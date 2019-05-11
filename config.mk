0bc371e03666ee937f7387572b4ff4661ab3b158
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-12 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    fca966d7b7 kernel: bump 4.9 to 4.9.172
    07720c392f kernel: bump 4.14 to 4.14.115
    bc85640cdc kernel: bump 4.19 to 4.19.38
    792c9fc8ca procd: update to latest git HEAD
    165d598521 netifd: update to latest git HEAD
    290a7dc0c7 procd: fix compile issue
    367813b9b1 ramips: mt7620: fix dependencies
    9cb3a3255e toolchain/nasm: update to version 2.14
    85017c40f4 build: add a config option for enabling a testing version of the target kernel
    4de0a61a62 kirkwood: enable sata port multiplier
    9b0eb4d260 ipq40xx: Fix reboot on EnGenius ENS620EXT
    6411eac5da ipq40xx: add factory image for EnGenius ENS620EXT
    ad62247800 base-files: improve lib/upgrade/common.sh
    0a11c87e46 mvebu: fix board_name condition in 79_move_config
    a4dc519888 ramips: fix R6120 factory image
    61f4ceb146 ath10k-firmware: update Candela Tech firmware images
    671d8752d1 ath10k-ct: Update to current version
    cb30971a44 lldpd: update to 1.0.3
    4582fe7c14 lldpd: add option to edit hostname
    d8ec6dee49 ath79: Add missing read-only properties
    bdaaf66e28 utils/spidev_test: build package directly from Linux
    9b1239451d Kernel: Activate CONFIG_HARDENED_USERCOPY
    32eb66881c kernel: Activate CONFIG_ARM64_SW_TTBR0_PAN
    7099bf0926 kernel: Remove CONFIG_DEBUG_RODATA and CONFIG_DEBUG_SET_MODULE_RONX
    46af22de16 kernel: Remove CONFIG_COMPAT
    57ec21ef3d kernel: Activate CONFIG_SYN_COOKIES for all targets
    d4ad7e6548 kernel: Do not set CONFIG_DEVMEM or CONFIG_DEVKMEM
    75eb8a146d kernel: Deactivate CONFIG_BINFMT_MISC
    2164a7bf15 x86: Activate CONFIG_X86_SMAP
    f7426b13fa cns3xxx: Activate CONFIG_CPU_SW_DOMAIN_PAN
    87ac3e801d gemini: Make kernel text and rodata read-only
    747587ec61 kernel: Reorder generic configuration
    3bc8ed91d4 generic-4.19: Backport spi-nand support for GigaDevice A/E
    e49b6bb618 xfsprogs: Replace valloc with posix_memalign
    0b26382533 uClibc++: Update to 0.2.5
    aecd0e09c9 toolchain/binutils: use default Host/Configure rule
    f0c2bac252 mvebu: remove unnecessary code building dtbs
    ef384c68e7 mvebu: image: stack repeated variables
    fb005d688e mvebu: image: introduce BOOT_SCRIPT variable
    9509a6df0b mvebu: image: improve readability of device recipes
    30b5554411 mvebu: image: don't create unnecessarily shell variables
    a39d2a8053 mvebu: align device names to vendor_device format
    ee96fa15b1 mvebu: use device-tree board detection
    de3387654b mvebu: add images for additional ESPRESSObin boards

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=476054102791d395af7225e47e6024573bebf2fe
+OPENWRT_COMMIT=de3387654b5b65dcd9029d68c8c2fab29f6fef84
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

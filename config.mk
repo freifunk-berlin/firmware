50d45c8e9462e3a0fce4dea6d6c2985cb7968baa
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-16 00:10:14 +0200

    Update to HEAD of OpenWrt-master
    3611cfe73d brcm63xx: fixup broken patch after kernel bump
    02d4d36d4b iperf: Update to version 2.0.13
    e669cf7f6a strace: Update to version 5.0
    a489f72ab5 valgrind: Update to version 3.15.0
    5ee62b23f8 valgrind: Add support for ARM64 architecture
    9ab4d74b5f kernel: Move some DSA config options to generic config
    77401cdc40 octeon: Refresh kernel configuration
    529c3477b9 octeon: Deactivate MIPS O32 and N32 support
    38666e8ae4 malta: Deactivate MIPS O32 and N32 support
    172b02c05f linux-firmware: update to 20190416
    5cd49395d3 lantiq/xrx200: enable initramfs images
    2f97797471 nftables: Fix compilation with uClibc-ng
    eb7eed416b mvebu: add kmod-i2c-mux-pca954x for Turris Omnia
    e7756974aa tegra: add vendor string to device name
    f32e05c541 ar71xx: Fix network setup for TP-Link Archer C25 v1
    0aa59e81b3 ath79: archer-x7-v5: remove confusing ar8327 initvals for LEDs
    e9652e1696 ath79: fix pinmux for ar933x devices
    33ce2ad3e7 ath79: Add missing reset button for TP-Link CPE210 v2 and v3
    71ab2c9d17 imagebuilder: new DISABLED_SERVICES make variable

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=86fd8cb435daf05f52468daabd41a363917485b7
+OPENWRT_COMMIT=71ab2c9d179652d9b528817ec04d3f1dd3b4b1df
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

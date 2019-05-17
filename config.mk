ebb8f922c8261de380a066b4ae7e1760a36b4480
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-05-17 20:38:24 +0200

    packages: rename default-flavor to notunnel
    in https://github.com/freifunk-berlin/firmware/issues/534 there was some discussion
    about meaningful names.

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
+PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=df6e8c8771d5457048a5cb1e510c6bc73bd4c173
 SET_BUILDBOT=env

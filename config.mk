d1d122d649809a495e1b527406eb9e6a8984c452
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-20 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    2d0c0abf5d apm821xx: WNDR4700: remove obsolete sd-card change detection
    df6e8c8771 uboot-fritz4040: Add host flags for host compiler

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a7967bada914c952efff77d6ca2c72d1f7c19c83
+OPENWRT_COMMIT=df6e8c8771d5457048a5cb1e510c6bc73bd4c173
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

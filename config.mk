d8f2d641abc8e3829723ba0147192074f6c8100b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-22 00:10:19 +0200

    Update to HEAD of OpenWrt-master
    107dc4326c generic: ar8216: add mib_poll_interval switch attribute
    01c0d7f86f generic: ar8216: group MIB counters and use two basic ones only by default
    2c26dc7b41 netifd: add support for the new ar8xxx MIB counters settings
    aac8b52184 base-files: add support for the new ar8xxx MIB counters settings
    6648e9458f ath79: set mib-poll-interval on mdio0 attached ar83xx switch
    00d14635db apm821xx: set mib-poll-interval on emac0 attached ar83xx switch
    73b21559ce ar71xx: set mib-poll-interval on devices with ar8xxx switch
    4318bba0e9 ath79: add leds migrations for archer-c7-v5
    049748e87e uboot-imx6: bump to 2019.04 and refresh patches
    2f1a11d008 imx6: apalis: fix config survival after sysupgrade -n
    0f6b944c92 ath79: glinet_gl-ar750s: Add USB power & microSD
    00324e434e kernel: bump 4.9 to 4.9.177
    f442b306f9 kernel: bump 4.14 to 4.14.120
    8eb7cf0cd7 kernel: bump 4.19 to 4.19.44
    4da5ba4a6b iwinfo: update to latest git HEAD
    8357b8131c ar71xx: wr940 v4/v6: correct lan interface mac address
    cde52cb486 base-files: add console to inittab

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=df6e8c8771d5457048a5cb1e510c6bc73bd4c173
+OPENWRT_COMMIT=cde52cb4866fe8cf4a19bb2bb32ff7802e542e19
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

6b2e0f46751d067ec0fb4af3de95597220a5ec86
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-25 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    0111b86f1d bc: update to 1.07.1
    33b81b5721 Revert "bc: update to 1.07.1"
    4bb9af48ca lua: lnum: fix strtoul based number parsing
    8a83f17b24 gemini: Support sysupgrade on DIR-685
    f21c8577bb glibc: update to latest 2.27 commit (BZ #24531)
    0293aa72d1 uci: fix heap use after free (FS#2288)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=cde52cb4866fe8cf4a19bb2bb32ff7802e542e19
+OPENWRT_COMMIT=0293aa72d1bfe732b826041b18ff35d2b4d31272
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

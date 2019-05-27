91ffb4f6c09ead5fe7db1ce2d4fba0e301afa1fd
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-28 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    c4abaa345c Revert "base-files: add console to inittab"
    ace241014c ethtool: bump to 5.1
    470f5b31e3 464xlat: don't set default firewall zone to wan
    f54611b06d map: don't set default firewall zone to wan
    7d77879236 curl: bump to 7.65.0
    6d6985ff0e ath79: add support for WD My Net N750
    99c52251cc ath79: ecb1750: additional dts fixes

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=0293aa72d1bfe732b826041b18ff35d2b4d31272
+OPENWRT_COMMIT=99c52251cc106fca754bdb184ed02422dce15bdc
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

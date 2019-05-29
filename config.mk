5365af73f36b290a1fa826cb0d06519a52982759
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-05-30 00:10:14 +0200

    Update to HEAD of OpenWrt-master
    6636171bed netifd: fix missing ip rules after network reload (FS#2296)
    bcb6fc2f84 kernel: bump 4.9 to 4.9.178
    35551e58db kernel: bump 4.14 to 4.14.121
    b97d44af67 kernel: bump 4.19 to 4.19.45
    8128a7e4fc busybox: fix: ip addr flush hangs when run by non-root user
    62940df3a9 procd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=99c52251cc106fca754bdb184ed02422dce15bdc
+OPENWRT_COMMIT=62940df3a9b6d5af2b33013efaf0bc72e5b23972
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

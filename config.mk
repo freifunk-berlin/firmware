a1a8e106c83114022795b25a6a39dc861666dd78
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-06-05 01:47:20 +0200

    Revert "config.mk: drop openvpn images"
    This reverts commit 434fa8c2a10765d134d85cfc5dbdd54b7158588a.

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger backbone
+PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=a1210f88882415f629cec272a0017fcb5a2ced3c
 SET_BUILDBOT=env

a96b4d23d042107cb0fef088b8d71883c86504cb
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-05 13:01:44 +0200

    Update to HEAD of OpenWrt-master
    593b487538 wireguard: bump to 0.0.20190601
    b8236e102c ath79: fix QCA955x GMAC register size
    9148cbcb7b apm821xx: WNDAP6X0: convert to interrupt-driven gpio-key driver
    651adad603 apm821xx: MR24: convert to interrupt-driven gpio-key driver
    1544df0d89 apm821xx: MX60: convert to interrupt-driven gpio-key driver
    dfa86b01e5 apm821xx: backport accepted linux-crypto patches to 4.14
    06e63aad57 apm821xx: backport accepted linux-crypto patches to 4.19
    a0f7bdfc71 kernel: bump 4.9 to 4.9.179
    8f6fd53db9 kernel: bump 4.14 to 4.14.122
    405e08bee6 kernel: bump 4.19 to 4.19.46
    ff88d94372 kernel: bump 4.9 to 4.9.180
    794771afde kernel: bump 4.14 to 4.14.123
    3209f5ae3d kernel: bump 4.19 to 4.19.47

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a1210f88882415f629cec272a0017fcb5a2ced3c
+OPENWRT_COMMIT=3209f5ae3d54e6f028e2e54314180a1be5b4896e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

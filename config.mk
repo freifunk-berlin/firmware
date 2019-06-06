44c2371d8456b7eb62e735992276cca9903b6361
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-07 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    ef7aa03bdb libunwind: bump to version 1.3.1
    52156b4968 kernel: generic: remove broken and obsolete phy_ethtool_ioctl
    bf58bc3bd8 ramips: ethernet: remove unused SIOCETHTOOL ioctl handling
    5fee0a0923 ath79: ag71xx: remove unused SIOCETHTOOL ioctl handling
    eafe0611d9 ar71xx: ag71xx: remove unused SIOCETHTOOL ioctl handling
    206af613b2 ath79: ag71xx: update ethtool support
    971d358579 ar71xx: ag71xx: update ethtool support
    9b7abd9fe2 ramips: mt7620: fix 5GHz WiFi LED on DWR-118-A1
    f84715d463 ath79: rework LED configurations for tplink,archer-d50-v1
    32817580e7 ath79: ag71xx: defer probe if of_phy_connect failed
    8dde11d521 ath79: dts: drop "simple-mfd" for gmacs in SoC dtsi
    c3a8518918 ath79: fix default config for devices with eth0/eth1 swapped
    046263095e ath79: Add support for ZBT-WD323
    64493d42b4 ath79: add support for 8devices Carambola2 development board
    0ab9f283f7 ramips: add support for TOTOLINK LR1200
    df34f29fcc kernel: generic: make kernel-debug.tar.bz2 usable again
    66d1c29655 kernel: bump 4.19 to 4.19.48
    dc8ec266dd rpcd: update to the latest git head
    7c50182e0c kernel: backport act_ctinfo
    f664d560df rpcd: fix init script reload action
    24e09bac48 Revert "kernel: backport act_ctinfo"
    94464cfca2 build: fix external module symbol collection if build_dir is a symlink
    d1a015f8ca brcm63xx: DVA-G3810BN/TL: Fix the WAN ethernet port
    239b79f668 kernel: add package for atusb wpan module
    65adf759b5 kernel: handle CFQ_GROUP_IOSCHED/CGROUP_HUGETLB in config-4.14
    d32ca55492 layerscape: remove POSIX_MQUEUE configs
    5dd307afef layerscape: update fman-ucode to LSDK 19.03
    1efc6f3515 layerscape: update ls-dpl to LSDK 19.03
    584611e076 layerscape: update ls-mc to LSDK 19.03
    f4f4b053b9 layerscape: update ls-rcw to LSDK 19.03
    fbb865099b layerscape: update ppfe-firmware to LSDK 19.03
    4b4b686b1d layerscape: update u-boot to LSDK 19.03
    9ad7c53383 layerscape: update restool to LSDK 19.03
    b4b53cd39b layerscape: drop armv8_32b support
    f7f1f39c34 layerscape: add rcw packages for ls1043ardb/ls1046ardb SD boot
    17dcbe1b8e layerscape: add ARM Trusted Firmware package
    c07d3302b3 layerscape: convert to use TF-A for firmware
    8468bf04d0 layerscape: drop ppa package
    639d127b83 layerscape: fix u-boot bootcmd
    5159d71983 layerscape: update patches-4.14 to LSDK 19.03
    b8a72dfd28 kernel: backport act_ctinfo

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=3209f5ae3d54e6f028e2e54314180a1be5b4896e
+OPENWRT_COMMIT=b8a72dfd28e510614121acae2e8cde8b28d07597
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

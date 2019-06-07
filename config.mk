1b705eac799c8fa00572b58b9d3ba74218790ef4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-08 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    021a9b4cb9 iproute2: add tc action ctinfo support
    de1431b589 kernel: re-add bridge allow reception on disabled port

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b8a72dfd28e510614121acae2e8cde8b28d07597
+OPENWRT_COMMIT=de1431b5898d104f185deadee82851eb912320e9
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

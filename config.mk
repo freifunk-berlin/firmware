8d960f468e7fdcb50e5ed2bebe876402af39d058
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-10 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    0f4e65ec7b tools/ccache: update to 3.7.1
    10011f91c5 busybox: add ALTERNATIVES for brctl
    6b762dd75f netifd: xfrm tunnel support
    0299a4b73e dnsmasq: skip options that are not compiled in
    33ccfe0e14 gpio-button-hotplug: use pr_debug and pr_err
    27d234a345 gpio-button-hotplug: fix wrong initial seen value
    6c5bfaac84 gpio-button-hotplug: gpio-keys: fix always missing first event
    0e1d07af1d kirkwood: image: fix unwanted 2nd inclusion of kernel
    ae3e232b11 netsupport: add xfrmi interface support

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=de1431b5898d104f185deadee82851eb912320e9
+OPENWRT_COMMIT=ae3e232b11ac7d76d6178ea308b3356d1baf33f0
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

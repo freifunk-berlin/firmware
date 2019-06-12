46aa301a3a9f1f3b43cdccc5b1f13a22fc07ebcf
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-06-12 22:42:43 +0200

    switch to upstream-branch OpenWrt-19.07

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=1867f108075a875c3334d827beff74bc6c24f2f6
+OPENWRT_COMMIT=b344ca9ca68322a023414e7e38ddd4d81aa8d378
 SET_BUILDBOT=env
 MAKE_ARGS=

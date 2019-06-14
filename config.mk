f2474eababd9ae798ee9d83b706148c28adf971f
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-15 01:20:32 +0200

    Update to HEAD of OpenWrt-19.07 branch
    c4e727f01c dnsmasq: move feature detection inside a shell func
    000d400baa kernel: drop everything not on kernel version 4.14
    4089df4f4b kernel: bump 4.14 to 4.14.125 (FS#2305 FS#2297)
    f96fbf0328 iwinfo: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b344ca9ca68322a023414e7e38ddd4d81aa8d378
+OPENWRT_COMMIT=f96fbf03281a69dc48cfea90044b21e30c15b7c7
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

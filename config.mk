ad8d80c81f098ff65a810ef1a5adc063de6f6757
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-15 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    f2f7cc67e5 kernel: bump 4.9 to 4.9.181
    a7e68927d0 kernel: bump 4.14 to 4.14.125 (FS#2305 FS#2297)
    11b4d29ef5 kernel: bump 4.19 to 4.19.50
    c12bd3a21b iwinfo: update to latest git HEAD
    38b22b1e70 nghttp2: deduplicate files in libnghttp2
    fcb41decf6 config: enable some useful features on !SMALL_FLASH devices
    35a70d6262 f2fs-tools: fixup SPDX license
    49b3dcb2ab ath10k-ct: Update to 2019-06-13
    ff2382e36c ath10k-firmware: update Candela Tech firmware images
    55fcc77072 netifd: update to latest git HEAD
    09c6885ce7 kernel: add missing symbol to 4.19 config

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=04b45d3a31fac45c472ad3c31d98268d1c309763
+OPENWRT_COMMIT=09c6885ce789c57be6c2fa6a3e354d4329945714
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

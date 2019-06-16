3ef43e2e8a318f6670372b69e135c74bc8273500
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-16 12:49:14 +0200

    Update to HEAD of OpenWrt-19.07 branch
    8386f975a9 kernel: mt29f_spinand: fix memory leak during page program
    918f4c8887 mac80211: ath9k: Increase allowed antenna gain to 6 dBi
    af76c19d53 mac80211: ath10k: fix max antenna gain unit
    06222ce73e mac80211: ath10k: adjust tx power reduction for US regulatory domain
    f1d4da3029 ath10k-ct: fix max antenna gain unit
    5913543190 ath10k-ct: adjust tx power reduction for US regulatory domain
    78049d448c iwinfo: update PKG_MIRROR_HASH
    158a716215 mac80211: refresh patches

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=f96fbf03281a69dc48cfea90044b21e30c15b7c7
+OPENWRT_COMMIT=158a71621577c6e52dc8539a773ba62e93ed5a1f
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

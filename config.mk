e45bf01a79940e3ce8c23dc6b055292760e2b3f4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-21 01:03:40 +0200

    Update to HEAD of OpenWrt-19.07 branch
    c52054e568 mac80211: brcm: backport remaining brcmfmac 5.2 patches
    4fce09c1ec kernel: backport 4.18 patch adding DMI_PRODUCT_SKU
    1d1dca45ea mvebu: fixes commit 4089df4f4b0d
    c26420b914 kernel: bump 4.14 to 4.14.126
    17fc5375f0 kernel: bump 4.14 to 4.14.127
    76c963bb01 ar71xx: base-files: fix board detect on new MikroTik devices
    9ed98198cb kernel: bump 4.14 to 4.14.128
    4e925d0b82 base-files: fix uci led oneshot/timer trigger
    136c8a889f ath79: Add SUPPORTED_DEVICES for Archer C7 v1/v2
    7888adb0aa kernel: update act_ctinfo
    2cb8ae2079 iproute2: update ctinfo support

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=158a71621577c6e52dc8539a773ba62e93ed5a1f
+OPENWRT_COMMIT=2cb8ae20790cccf89deb15b2e0f08f36b5035cfd
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

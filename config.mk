6f0cf530ff819c837cbb5a1691ad7d016b16c462
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-27 00:38:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    69b988c994 mac80211: update WDS/4addr fix to the version accepted upstream
    b8fc9c1580 kernel: bump 4.14 to 4.14.129
    a471d8c595 ipq40xx: Linksys: sysupgrade: Ensure OEM volumes are removed
    43b8e5b120 mpc85xx: Use uncompressed kernel on the HiveAP-330
    dbe5730bf9 apm821xx: fix bogus key-presses on boot
    eee336ee4a kernel: bump 4.14 to 4.14.130
    94a1999b4f layerscape: drop pause frame support for aquantia phy

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2cb8ae20790cccf89deb15b2e0f08f36b5035cfd
+OPENWRT_COMMIT=94a1999b4feed6cfc57513252407ea85fc8e91fb
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

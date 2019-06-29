0bf8f780e4ee04c021f91cc9b1cf95c7b4bb6b8e
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-06-30 00:10:11 +0200

    Update to HEAD of OpenWrt-master
    8b74cc45be kernel: bump 4.9 to 4.9.184
    0d71c7a57c kernel: bump 4.14 to 4.14.131
    1ffca55456 uqmi: bump to latest git HEAD
    683341d98f kernel: generic: add missing 4.19 config symbol
    ef5b7aa6c3 build: image: make image padding OS agnostic
    c0c5c63514 lua: clean up host patch fuzz
    fe59b46ca7 lua: include version number in installed files
    6b161bb8d5 lua5.3: package Lua 5.3 version
    24645c0ee1 lua: fix build with MacOS's make
    1cd46d2e4f lua5.3: fix build with MacOS's make
    026714613d u-boot-mvebu: bump to 2019.04
    95c1dc5e86 mvebu: Fix platform.sh for non-generic espressobin versions
    c2e819537f mvebu: add initial support for uDPU board
    9e780ed5f7 base-files: add network_get_uptime() to /lib/functions/network.sh

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=3f68cffd276df5b164ea0c7ecafb8fa062aa7a41
+OPENWRT_COMMIT=9e780ed5f7b834e06ec5c82323731917aef4ee53
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

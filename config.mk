df5ea06980d630f15ff4bd65d6b90d09cfd8cf09
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-03 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    17ae3eb9ff lua5.3: drop unwanted & unneeded PROVIDES
    b50aef8272 gemini: Drop switch kmod and swconfig
    18e2053bec gemini: Add StorLink SL93512r images
    7a1b575ac4 scripts/feeds: add src-include method
    cc1147072d ramips/leds: use devicetree alias led_wlan
    3fd97c522b ramips: add support for TP-Link TL-WR841n v14
    fdd0a8d491 Make linux kernel builds reproducible when BUILDBOT selected
    299f6cb2da iptables: update to 1.8.3
    7c23f741e9 wireguard: bump to 0.0.20190702

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9e780ed5f7b834e06ec5c82323731917aef4ee53
+OPENWRT_COMMIT=7c23f741e97f6645bb5cd662a4943796a344b26a
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

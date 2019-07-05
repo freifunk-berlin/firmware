c7f0c45523837989968f0002eb273300d4ba9a26
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-05 10:24:17 +0200

    Update to HEAD of OpenWrt-master
    47a984477b lua5.3: stage Lua headers in proper location
    340df72e07 scripts: time.pl: Don't print the time on stderr
    ab02584685 ath79: Use -v1 suffix for TP-Link WDR3600/4300
    8c7d6c47a7 ar71xx: fix ath79/rb4xx IRQ initialization on kernel 4.14
    8eb63cb7df uqmi: add mtu config option possibility
    c06f2a2dcb uqmi: fix indentation style and boundary
    537b801c54 base-files: supress service restart of umount
    313444a79e comgt: add delay option for 3g proto
    1d4c4cbd20 openvpn: fix handling of list options
    030d3f48e5 lantiq: enable STP where referenced
    c3e31b6a9b build: skip kernel stack validation when building on macOS
    bf21b6e44d lantiq: ltq-tapi: fix compatibility with Linux 4.15+
    eee1b34ce6 lantiq: copy target to kernel 4.19
    f0c740650b lantiq: make patches apply
    eb72439dbb lantiq: 4.19: increase usb reset timeouts
    961b8efb5b lantiq: 4.19: load PCIe driver after PCI driver
    7305e2057a lantiq: 4.19: fix panic when using the DMA API
    e6867a5283 lantiq: switch to kernel 4.19
    cb689f2723 lantiq: drop kernel 4.14 support
    8c3dbaf8b4 ath79: split DEVICE_TITLE in multiple variables
    5bba6b4e6b ath79: Move DEVICE_VENDOR for NETGEAR to parent definition
    b1100fc586 ath79: Correctly use DEVICE_VARIANT for NETGEAR WNDR3700
    92f83abc5e mt76: update to the latest version
    917eeaf26b iproute2: update to 5.1.0

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=7c23f741e97f6645bb5cd662a4943796a344b26a
+OPENWRT_COMMIT=917eeaf26bca5342817419d0c360301896932476
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

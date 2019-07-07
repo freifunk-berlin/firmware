619cfe54129ea265b5e2e575fc7c604949a0130d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-08 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    63e3c3d2a2 ath79: Add DEVICE_VENDOR for Ubiquiti Routerstation
    ee66fe4ea9 ramips: convert DEVICE_TITLE to new variables
    df13384c56 lantiq: dts: move memory node to board dts
    4053dd3f82 lantiq: dts: add reg if unit address is set
    fcac34d01d lantiq: dts: drop superfluous unit address
    35e45b742d lantiq: dts: fix malformed unit addresses
    e55827b7a5 lantiq: dts: fix size cells
    7685ed59c5 lantiq: dts: add missing size and address cells
    2ec59c173f lantiq: dts: move partitions into partition node
    fdcfd2b232 lantiq: dts: drop superfluous address and size cells
    5eb7e513a5 lantiq: dts: use lower case for hex values
    85debe661e lantiq: dts: fix vmmc unit and register address
    3b215a01bb lantiq: dts: move localbus childs to dts
    0c93c09e6d lantiq: dts: fix unit address mismatch warning
    627b97f57d lantiq: dts: drop empty falcon devicetree node
    617622cb3c lantiq: dts: drop falcon clock node
    a4bdae7cf2 lantiq: dts: use generic node names
    92b768d5f4 lantiq: fix etop compiler warnings
    aac25bdba5 kernel: 4.19: Backport usb dwc2 lpm mode hw check
    787922682a gdb: Remove !arc dependency
    243765e389 gdb-arc: Remove
    7fad676115 toolchain: Don't force GCC8 on ARC
    7f79882d44 ath10k-firmware: update board-2.bin for community firmwares
    2792daab5a wolfssl: update to 3.15.7, fix Makefile
    ff69364ad8 wolfssl: update to 4.0.0-stable
    94d131332b hostapd: adjust removed wolfssl options
    b683f1c36d mpc85xx: Use gzip compressed kernel on HiveAP-330
    8592602d0a base-files: Really check path in get_mac_binary
    c017ead66e ramips: Fix compatible for YUKAI Engineering BOCCO
    ce8027ed29 libnftnl: bump to version 1.1.3
    f62fe6fdba gemini: Fix device name for StorLink SL93512r
    dc3cf94cb2 gemini: Add SL93512r missing ImageInfo file
    c0d93432f2 zram-swap: Fix busybox dependency check
    b062c90f47 zram-swap: Add zram compaction and statistics info output
    afa5ce2493 busybox: enable swapon/off by default to make it consistent with mkswap
    a9deed62af zram-swap: Add extra commands for status/compaction
    d5a38725f8 ath79: increase kernel partition size for CPE610v1
    418f826c2c kernel: Fix MIPS bounds check virt_addr_valid
    7c640c2960 ath10k-firmware: Fix mirror hash
    8ef6240fa6 kernel: Add missing includes mtdsplit_*.c

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=917eeaf26bca5342817419d0c360301896932476
+OPENWRT_COMMIT=8ef6240fa61e65eb7b4386e3ea683b04e1dbf00f
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

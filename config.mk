fa78ecff4bc3974b3caa8a4b6bd1e3b294bdeae4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-10 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    f58ca6ee57 base-files: drop unused jffs2_copy_config()
    ccf8a8e863 bcm53xx: sysupgrade: call nand_do_upgrade() from platform_do_upgrade()
    ea4e1dac71 base-files: drop support for NAND upgrade in platform_pre_upgrade()
    5c094ff660 kmod-sched-cake: drop out of tree package, use kernel version
    42b3a3a89b iperf3: update to 3.7
    c3253b52a2 zynq: convert to multi-variable device title
    301a78d1d5 ath79: Add missing IMAGE_SIZE for Etactica EG200
    6fac9077c6 tools/gengetopt: Update to 2.23
    d51f53b5ba util-linux: Update to 2.34
    5dc7d63d0a netsupport: move out mqprio from kmod-sched
    653e05d27f usbreset: Add missing header
    7a8d3432c7 ramips: use upstream RAW_APPENDED_DTB instead of our OWRTDTB
    483b9a92b3 kernel: x86: enable NVME support for 64 bit target
    b31f9190c3 netsupport: move out sch_cake from kmod-sched
    0051fff171 kernel: bump 4.14 to 4.14.132
    fa2435903f kernel: bump 4.19 to 4.19.57
    9ef53b93cb rb532: Fix missing DEVICE_TITLE
    6ed3349308 base-files: Fix path check in get_mac_binary
    1174b94bc9 ath79: Fix whitespace errors in 11-ath10k-caldata

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=8ef6240fa61e65eb7b4386e3ea683b04e1dbf00f
+OPENWRT_COMMIT=1174b94bc98db9730cec8501397cf997b6f7b35c
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

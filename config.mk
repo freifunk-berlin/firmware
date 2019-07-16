3c5f6f81cd2a6de24eb9432fb7b169c3a47786a2
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-17 00:10:11 +0200

    Update to HEAD of OpenWrt-master
    9e57b62783 include/package.mk: Add support for src-checkout/ folder
    7d96c301d6 kernel: cherry pick patch removing __linux__ check
    08e0db3e98 oxnas: convert DEVICE_TITLE to new variables
    c82a861d90 ath25: split up DEVICE_TITLE
    5850487473 kirkwood: split up DEVICE_TITLE
    87552e5400 ipq40xx: convert to multi-variable device title
    472dbaba81 sunxi: convert to multi-variable device title
    c2b1baf767 ramips: add missing DEVICE_VENDOR
    dcb5cf85c8 ath79: fix missing DEVICE_VENDOR for some devices
    d93969a13a ramips: Improve compatible for TP-Link Archer devices
    9f99000165 ramips/mt7620: Synchronize Makefiles with DTS compatible
    2f2a319f82 ramips/mt7621: Synchronize Makefiles with DTS compatible
    c2334ad60d ramips/mt76x8: Synchronize Makefiles with DTS compatible
    83efa8329b ramips/rt288x,rt3883: Synchronize Makefiles with DTS compatible
    7dd3a234be ramips/rt305x: Synchronize Makefiles with DTS compatible
    402138d12d ramips: Derive DTS name from device name in Makefile
    48047b3a5c ramips/mt7620: Name DTS files based on scheme
    7196ca1d49 ramips/mt7621: Name DTS files based on scheme
    b97a99256f ramips/mt76x8: Name DTS files based on scheme
    6a104ac772 ramips/rt288x,rt3883: Name DTS files based on scheme
    3b0264eddb ramips/rt305x: Name DTS files based on scheme
    e8d2d97498 ramips: Remove base-files/lib/ramips.sh
    1096d1b697 uboot-envtools: Apply ramips device renames
    b4588c8538 kernel/om-watchdog: Apply device renames from ramips
    5d50155f14 ramips: Reapply sorting to board.d files
    447deed52a ramips: Sort all subtarget Makefiles
    98d04dc4cf ramips: fix bogus MTK_SOC (typo)
    6d91ddf517 ramips: mt7621: add support for memory detection
    a2c19f1d2f ramips: dts: drop memory nodes
    087e14ab59 ramips: mt7621: merge two variants of MQmaker WiTi together
    f7edd94a65 base-files: move stage2 upgrade to separated file
    53daa51cf2 ramips: fix mqmaker witi case in fixup-mac-address
    c52ca08d40 lua5.3: build shared library
    14e0e4f138 ramips: add support for ASUS RT-AC57U
    e05310b9b8 mac80211: Do not build b43legacy on BRCM47xx mips74 subtarget
    ba5878f056 mt76: update to the latest version
    9a72e7f601 procd: update to latest git HEAD
    f1875e902d mt76: revert an accidental leftover debug change
    4295485719 brcm2708: add linux 4.19 support
    6c3e7d5ea0 brcm2708-gpu-fw: add support for Raspberry Pi 4
    aa00ac44d9 linux-firmware: add RPi SDIO NVRAM packages
    9e8932c17f brcm2708: switch to linux-firmware SDIO NVRAM
    f781582828 brcm2708: Add Raspberry Pi 4 support
    27f3f493de gpio-button-hotplug: unify polled and interrupt code
    ea44ed5f38 gemini: Stop enforcing command line
    cb3cd52694 ath79: add support for devolo WiFi pro 1750x
    cbae306815 fstools: add direct dependencies on libblobmsg-json and libjson-c
    285c83a004 rpcd: implement service_running() in init.d script
    1b937cb141 ubox: implement service_running() in log init.d script
    056d665ec2 tegra: split up DEVICE_TITLE
    c47eff0df3 libs/toolchain: remove eglibc remnant file
    7d97bc19f0 target/toolchain/files/wrapper.sh: simplify 'case'
    430d65c544 libroxml: bump to the 3.0.2 version
    4a06d62f0e mpc85xx: split up DEVICE_TITLE
    fae125781e ramips: mtk-mmc: mt76x8: check ESD_MODE before applying AGPIO_CFG
    a9360452f0 ath79: add support for Qihoo C301
    f65501e1c2 ath79: ar93xx/qca95xx: move gmac/wmac/pcie node out of apb bus
    d08b5bb344 ath79: add wmac migration for all ar93xx/qca95xx SoCs
    6d61fcfa9e ramips: Remove left-over mistyped device string from 02_network
    670ce65a44 mvebu: sysupgrade: drop platform_copy_config_sdcard() argument
    bf4eef46b4 orion: sysupgrade: change platform_check_image() args validation
    e2cd70d6b1 package: mtd: add fixseama command for ath79
    deb2c56b4c ath79: add seama header fixup for Qihoo C301
    2a7519e29d ar71xx: enable SGMII fixup on Mikrotik wAP AC
    20452a8db9 ar71xx: get rid of platform_pre_upgrade()
    d9593b0809 ramips: get rid of platform_pre_upgrade()

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=1174b94bc98db9730cec8501397cf997b6f7b35c
+OPENWRT_COMMIT=d9593b0809e42e9d75cc3ac4407ec30faaf70b15
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

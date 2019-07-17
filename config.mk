a72ab4e9dd49721545c9f8da3be93d6cb177befd
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-18 00:30:15 +0200

    Update to HEAD of OpenWrt-19.07 branch
    cb9db2a0c3 ar71xx: fix ath79/rb4xx IRQ initialization on kernel 4.14
    1b99051399 lantiq: enable STP where referenced
    acd2497cfb mpc85xx: Use gzip compressed kernel on HiveAP-330
    cd4fcd46f1 base-files: Really check path in get_mac_binary
    3167a57f72 wolfssl: update to 3.15.7, fix Makefile
    5ef6c20183 netsupport: move out mqprio from kmod-sched
    411e42cdc6 wolfssl: Fix package hash
    7f4510a251 kernel: bump 4.14 to 4.14.132
    65034359ee base-files: Fix path check in get_mac_binary
    955b393cf0 kernel: cherry pick patch removing __linux__ check
    87457ad81b mac80211: Do not build b43legacy on BRCM47xx mips74 subtarget
    f40f7b0fb5 kernel: bump 4.14 to 4.14.133
    519cdf377a kernel: delete leftovers from unused kernel versions
    b947de25a6 ar71xx: fix nand init issues on some rb2011 devices
    8936bdb269 opkg: bump to version 2019-06-14
    1c3587cad9 busybox: strip off ALTERNATIVES spec
    48662712b2 fstools: add direct dependencies on libblobmsg-json and libjson-c
    74b0b42fc6 libroxml: bump to the 3.0.2 version
    7a4acfce6d iptables: update to 1.8.3
    0254087e92 ath79: ar9330: add missing watchdog node
    c0a715f75e openvpn: fix handling of list options
    96cc390d88 ath79: Use -v1 suffix for TP-Link WDR3600/4300

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=4a7a8d93fa1920efbf42c5e98a637a396dac9a5c
+OPENWRT_COMMIT=96cc390d8808a79f920b6b3fd256cfeb6fd501b2
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

965918e4b0867711d2c24850d984fbc168f380d4
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-20 00:30:15 +0200

    Update to HEAD of OpenWrt-19.07 branch
    074c8e296c gpio-button-hotplug: fix 4.19 build breakage on malta/be64
    6d59f4eeb4 gpio-button-hotplug: unify polled and interrupt code

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=96cc390d8808a79f920b6b3fd256cfeb6fd501b2
+OPENWRT_COMMIT=6d59f4eeb4c3d49257379499c8a43eea058f4d51
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

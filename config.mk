a9f786aecf10506af543dd167109b6379f770f1c
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-20 00:10:21 +0200

    Update to HEAD of OpenWrt-master
    8b4bc7abe0 treewide: sysupgrade: don't use $ARGV in platform_do_upgrade()
    76e43c82b5 mvebu: sysupgrade: don't use $ARGV in platform_check_image()
    3f4c785a6b base-files: don't set ARGV and ARGC
    8650201f10 mac80211: add config tweak for tx bursting when using VHT
    4c46bbbd93 mt76: update to the latest version
    bb27cde257 x86: add modern network modules to Generic target
    5e9a6d08d2 imx6: apalis: fix sysupgrade paths
    0711b515f6 imx6: apalis: add missing jffs2reset to ramfs during sysupgrade
    57d096e2a9 kernel: generic: fix perf build breakage on 4.19
    16ac5c4fbd perf: simplify the build process
    cda123fe69 ramips: add MT7615 wireless support for ELECOM WRC-1167GHBK2-S
    0893f28e19 ath79: ar9330: add missing watchdog node
    4d3c8872f8 brcm47xx: fix 831-old_gpio_wdt.patch for linux 4.19
    261df949fa openvpn: add new list option tls_ciphersuites
    fd104daa2f mtd: add CRC signature to RedBoot partition map
    abf09cfddb ath79: support "rgmii-id" PHY mode in ag71xx
    a9fa91d200 ath79: widen RedBoot directory search and add PHY
    bc18c2bfe7 ath79: add support for Bluesocket BSAP1880 board
    d6198d8625 mtd: cleanup unused code and variables in fis.c
    8c51ddeff0 ramips: add support for TP-Link RE650 v1
    6fde0b735c ath79: add support for TP-Link RE355 v1 and RE450 v1
    8e384ba830 ath79: add support for Trendnet TEW-823DRU
    cad4132e00 ath79: add support for TP-Link CPE510-v2/v3
    6d313da6dc ath79: Add support for TP-Link Archer C60 v1
    974d6958a7 ath79: Add support for TP-Link Archer C60 v2
    d616b2c906 ath79: modify device name of I-O DATA WN-AC1600DGR2
    0b2c42ced2 mac80211: Update to version 5.2-rc7
    8f61b4cac4 ath10k-ct: update to version 2019-06-13
    10fe5ca362 ath10k-ct: remove patches for old versions
    bc5b2bcd9c ath10k-ct: switch to version 5.2
    9c0c1c4401 ath10k-ct: Revert back to version 4.19
    83e60f0df6 firmware-utils: mkfwimage: enable extra compiler checks and fix them
    9401780c2c firmware-utils: mkfwimage: provide human readable error
    90b7dbf7d8 firmware-utils: mkfwimage: fix more errors
    04cb651376 firmware-utils: mkfwimage: fix more errors reported by gcc-6/7/9

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d9593b0809e42e9d75cc3ac4407ec30faaf70b15
+OPENWRT_COMMIT=04cb651376f94388a5a7c2c9a983553f8b909824
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

cd6abb98fa4d2f1ed421c04c0d397aee9039fa0f
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-23 00:10:12 +0200

    Update to HEAD of OpenWrt-master
    ff7909f69e firmware-utils: mkfwimage: fix build failure on macOS with gcc 9
    3f1e8c0131 tools: libressl: update to 2.9.2 version
    db8e08a5a4 mac80211: brcm: backport first set of 5.4 brcmfmac changes
    fc2df4f705 curl: update to 7.65.3
    3b30ff2975 layerscape: sysupgrade: get rid of platform_pre_upgrade()
    1cbe0d659c treewide: sysupgrade: get rid of platform_nand_pre_upgrade()
    790692dde2 base-files: drop support for the platform_nand_pre_upgrade()
    8d6d227bb6 tools: libressl: fix build on MacOS
    11617bcb3b netifd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=04cb651376f94388a5a7c2c9a983553f8b909824
+OPENWRT_COMMIT=11617bcb3be6778d5427723a09922aae50956a8c
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

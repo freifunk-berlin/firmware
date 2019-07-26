cbc38b2c6874fc1684a0a5bb8cceed203922784b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-27 00:10:19 +0200

    Update to HEAD of OpenWrt-master
    0e6d97f961 ramips: enable Newifi D1 SD slot
    1282a63027 tools: libressl: fix compilation for non-glibc clib (FS#2400)
    209b368254 ath79: add support for D-Link DIR-842 C2
    09c33df76f mt76: fix kernel Oops by updating to the latest version
    273b803623 ath79: fix indent in 01_leds
    fe928c7a81 build: allow simple build system customization with local.mk
    b8249cef9f tfa-layerscape: fix fiptool host build
    fd5aa19480 ar71xx: update qca-usb-quirks patch
    c79c001b59 ar71xx: Archer C7 v1 LED names and RFKILL fixes
    a73934fc9a ar71xx: fix system LED names on Archer C5/C7
    65762cdd22 ar71xx: fix WLAN LED names for Archer C7
    12610a5620 Revert "build: allow simple build system customization with local.mk"
    1aad1d17ed iptables: add connmark savedscp support
    dba5a01358 kernel: netfilter add connmark savedscp support
    7d684b7673 ramips: add kmod-mt7615e to Phicomm K2P images
    e9eec39aac dnsmasq: backport latest patches
    cd91f2327f dnsmasq: improve insecure DS warning
    f30d588460 ramips: fix mac addresses used by Phicomm K2P
    b9cf1f6081 ramips/rt305x: fix firmware partition size for some 4MB devices
    7392af7520 ramips: fix ralink_default_fw_size_16M
    a6cd311b0b ramips: support IMAGE_SIZE in kiB for Build/trx
    7009f407f1 ramips/mt7620: fix IMAGE_SIZE for all devices
    7263838e69 ramips/mt7621: fix IMAGE_SIZE for all devices
    439435f59b ramips/mt76x8: fix IMAGE_SIZE for all devices
    1c611f1617 ramips/rt288x,rt3883: fix IMAGE_SIZE for all devices
    24ded1810b ramips/rt305x: fix IMAGE_SIZE for all devices
    363197311b ramips: remove default IMAGE_SIZE for ramips target
    9d87a58d34 ramips: remove ralink_default_fw_size_xxx variables
    f14a5c3f2f ramips: use kiB instead of B for IMAGE_SIZE
    ee5fce887a ramips: consolidate MAC address cases in 02_network
    f72d2ba60a ramips: consistently use lower-case factory partition label
    ee650ba46c ramips: remove needless setting of lan_mac to eth0 in 02_network
    679a01f34e ramips: drop an empty case in 02_network
    5897b2be21 ath79: remove lines-initial-states property
    e9216b3336 openwrt-keyring: update to Git HEAD
    5989a75cc4 tools/b43-tools/b43-fwsquash: convert to Python 3 with 2-to-3
    e785da815b scripts/dl_github_archive.py: convert to Python 3 with 2-to-3
    57bb89b57f scripts/jungo-image: convert to Python 3 with 2-to-3
    316c8b5503 tools/scons: switch to Python 3
    df0d555ea5 layerscape: convert to python3 for rcw
    19938c8de7 build: switch to Python 3
    3b68fb57c9 build: cleanup possibly dangling Python 2 host symlink
    ef3f868da0 wireless-regdb: update to 2019.06.03
    d3853d17a3 wireless-regdb: prefer python provided by make variable
    57d1c05ec9 wireless-regdb: set PKGARCH:=all
    5cf897779e autotools.mk: autoreconf: fix missing install-sh
    a1c6a316d2 ramips: add support for Fon FON2601
    83d5ca2186 tfa-layerscape: fix create_pbl and byte_swap host build
    9437012b9e tools/mklibs: update to 0.1.44 and convert to Python 3

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=11617bcb3be6778d5427723a09922aae50956a8c
+OPENWRT_COMMIT=9437012b9ee4dc550e42665b71902cf885169100
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

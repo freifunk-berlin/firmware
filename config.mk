796c78a607edb4f1852f5d860f6d4c271cc336a7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-07-31 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    cbe08e6b56 toolchain/fortify-headers: Update to 1.1
    8e466fb7e3 mac80211: brcm: update brcmfmac 5.4 patches
    6a7b201b6c mac80211: brcm: improve brcmfmac debugging of firmware crashes
    e09da0169a ar71xx: fix Mikrotik board detection
    5c661f5aaa Revert "netsupport: move out sch_cake from kmod-sched"
    4dcef8263e Revert "kmod-sched-cake: drop out of tree package, use kernel version"
    4bc02a421f iptables: fix connmark savedscp build
    8562e77953 wireless-regdb: fix Makefile indentation
    a421f33316 ath79: fix indent in TP-Link TL-WR842N v3 DTS file
    c3c3cd5e4a lantiq: use wpad-basic for boards with enough storage
    c6d41c320c scripts/ubinize-image.sh: fix buildbot breakage
    995bcc5329 tools/patch: apply upstream patch for CVE-2019-13636
    12840674d0 wireless-regdb: fix patch fuzz

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9437012b9ee4dc550e42665b71902cf885169100
+OPENWRT_COMMIT=12840674d0550e6f5aa077cd2b578988f0c07074
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

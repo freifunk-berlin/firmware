ffe4a9e24c9ba7b6e539fafd2c55c931ba68485d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-04 00:30:16 +0200

    Update to HEAD of OpenWrt-19.07 branch
    2938d6530a ath25: disable image generation for ubnt2 and ubnt5 devices
    299faa7ab7 x86: add modern network modules to Generic target
    4022035aaf mt7620: disable image generation for Netgear EX2700
    76a4c93f9d ar71xx: enable SGMII fixup on Mikrotik wAP AC
    5cd9b50d69 ath79: add support for devolo WiFi pro 1750x
    abe137ff5e ramips: add support for ASUS RT-AC57U
    bc0c0a5564 mac80211: brcm: backport first set of 5.4 brcmfmac changes
    5082c1b204 urngd: add micro non-physical true RNG based on timing jitter
    81c5a5c4b3 ubox: move getrandom into separate getrandom package
    f573d3de11 base-files: move urandom seed bits into separate package
    a624821474 build: add urandom-seed and urngd to default packages set
    5100629e32 urngd: move project to git.openwrt.org
    0a64b70e4e openwrt-keyring: update to Git HEAD
    94e9084e1e tfa-layerscape: fix fiptool host build
    e0f468864b tfa-layerscape: fix create_pbl and byte_swap host build
    d8c59fa699 mac80211: brcm: update brcmfmac 5.4 patches
    8efccf76f8 mac80211: brcm: improve brcmfmac debugging of firmware crashes
    3c65433186 ar71xx: update qca-usb-quirks patch
    c59e24ca30 ar71xx: Archer C7 v1 LED names and RFKILL fixes
    be14b1feae ar71xx: fix system LED names on Archer C5/C7
    5ec609815e ar71xx: fix WLAN LED names for Archer C7
    1172d809f2 ar71xx: fix Mikrotik board detection
    2b074654b0 ath79: make target source-only
    8c57d88fcc imx6: bump SDMA firmware to 3.5
    4fd61b6220 kernel: bump 4.14 to 4.14.134
    19f768f691 toolchain/musl: bump to version 1.1.23
    a654ac0d6e ar71xx: really fix Mikrotik board detection
    862352cdbd ar71xx: fix HiveAP 121 PLL for 1000M
    53b61baac6 scripts/ubinize-image.sh: fix buildbot breakage
    a941d39460 tools/patch: apply upstream patch for CVE-2019-13636

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6d59f4eeb4c3d49257379499c8a43eea058f4d51
+OPENWRT_COMMIT=a941d39460b67d2d21e86d9c73d3e9b099b2d7fb
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

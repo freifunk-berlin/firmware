b187a50863a32efe1e2fa15797ac390465d589f5
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-09 00:30:18 +0200

    Update to HEAD of OpenWrt-19.07 branch
    bcbc7ba768 kernel: bump 4.14 to 4.14.136
    7a10c7b882 usign: update to latest Git HEAD
    4fd7a30c70 packages: apply usign padding workarounds to package indexes if needed
    ff550dae32 adb: fix build breakage on recent musl
    872cbcc628 config: introduce separate CONFIG_SIGNATURE_CHECK option

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=0f3d54f5b70c42c2b0f7802970dac123ea0f8689
+OPENWRT_COMMIT=872cbcc6280738fa9b5a7f9f7d49576405d2f18e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

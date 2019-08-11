b6edeb85cb2246d310ff19bf49831e358911882d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-12 00:30:15 +0200

    Update to HEAD of OpenWrt-19.07 branch
    33af038bec kernel: bump 4.14 to 4.14.137
    dda342abba ar71xx/ath79: ag71xx: fix sleep in atomic
    c8a6ce71e4 ar71xx/ath79: ag71xx: init rings with GFP_KERNEL
    62eae4805b ar71xx: ag71xx: init rings with GFP_KERNEL
    30798132bf odhcpd: fix lingering uloop socket descriptor
    af2ceb93d7 lantiq: use wpad-basic for boards with enough storage
    5355665cc8 lantiq: correct Fritz!Box 7412 button logic level
    b72fa3e4a8 lantiq: enable second VPE on Fritz!Box 7412
    61e51473c2 lantiq: add led-upgrade alias for Fritz!Box 7412

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=872cbcc6280738fa9b5a7f9f7d49576405d2f18e
+OPENWRT_COMMIT=61e51473c237db7a10c35a3da5a4eb492fa72a90
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

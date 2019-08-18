4d2ef152cf1d2cc2e2b4b794e8f89c6077d35592
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-19 00:30:13 +0200

    Update to HEAD of OpenWrt-19.07 branch
    46e9a9b04e tools/patch: apply upstream patch for cve-2019-13638
    bc138c5b72 kernel: bump 4.14 to 4.14.138
    ce41f468e1 Revert "ath79: make target source-only"
    d97c6d9f09 lantiq: unify Fritz!Box LED mappings
    45a26cb211 odhcpd: fix sending RA when link state brings bridge down
    342bcb739f mvebu: enable xHCI USB controller connected to PCIe
    e79f192c0a mvebu: sysupgrade: sdcard: fix platform_do_upgrade_sdcard
    6db454e9b6 kernel: Activate CONFIG_OPTIMIZE_INLINING
    0f0ed2451e odhcpd: fix closing of router socket
    ac7ccf739a mac80211: Update to version 4.19.57
    3809b6662d mac80211: Update to version 4.19.66-1
    b35e1360cd wolfssl: bump to 4.1.0-stable
    6868188101 bzip2: Update to 1.0.8
    3b34fcaf94 musl: ldso/dlsym: fix mips returning undef dlsym
    a2a311fc2b uhttpd: Fix format string build problems
    190c5626ff uhttpd: add direct dependency on libjson-c
    5d906a656e uhttpd: update to latest Git HEAD
    b2cb6bb48a musl: Fix CVE-2019-14697

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=61e51473c237db7a10c35a3da5a4eb492fa72a90
+OPENWRT_COMMIT=b2cb6bb48abbc0521fbd1a928c37f4846427a959
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

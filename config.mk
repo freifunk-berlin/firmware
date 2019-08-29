7c8ae7f2e8b676c78948a640cd0cb9dbaa0bd5c1
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-30 00:10:20 +0200

    Update to HEAD of OpenWrt-master
    b8b7d4cbca gcc: Update gcc 9.X to version 9.2.0
    91c84e87c2 mac80211: rt2x00: clear IV's on start to fix AP mode regression
    0851ce4ff9 elfutils: bump to 0.177
    164037983d scripts/dl_github_archive.py: fix python3 transition
    032e08a011 mac80211: remove TX_NEEDS_ALIGNED4_SKBS patch
    a886a0ecc8 mac80211: renumber subsys patches after update
    a0637718d5 mac80211: add new minstrel_ht patches to improve probing on mt76x2
    0441edfb7f mac80211: backport support for the IEEE80211_KEY_FLAG_GENERATE_MMIE flag
    bd01346bb4 firewall: update to latest git HEAD
    4ee3cf2b5a build: introduce ALT vendor/model/variant
    fd66687058 lantiq: split up DEVICE_TITLE
    58aab73797 image.mk: keep underscores when sanitize PROFILE
    149d1e7a61 ramips/mt76x8: add DEVICE_VENDOR for tl-wr840n-v5
    41f580b596 ramips: add device alias for Allnet ALL0239-3G
    2b1a6d263c procd: update to latest git HEAD
    b534ba9611 base-files: pass "save_config" option to the "sysupgrade" method
    b6f4cd57e1 treewide: sysupgrade: pass "save_partitions" option to the "sysupgrade" method
    0fcab46f97 lantiq: sort device definitions in image/Makefile
    ccf3bac6f3 lantiq: add device alias for Alpha ASL56026
    853e4dd306 ipqx0xx: add Generic subtarget
    4346de8d34 mac80211: rt2x00: import pending patches
    fb3574f123 ramips: add support for ADSLR G7
    921675a2d1 ath79: add support for gl-ar750
    f0992d7a30 mac80211: fix a regression in the minstrel_ht improvement patches
    bd098231ba iproute2: update to 5.2.0
    b8474dc42b kernel: 4.19: backport mx25u12835f support from linux 5.0
    52cbe6b9c0 kernel: add i2c-pxa driver
    7dff6a8c89 mvebu: uDPU: add sysupgrade support
    f4500fde1e kernel: refresh patches
    ac2f341036 make-ext4fs: update to HEAD of 2017-05-29 - eebda1
    e9d875a537 ath10k-ct: update to HEAD of 2019-08-14 - 9e5ab2
    8036345225 fstools: update to HEAD of 2019-07-01 - 1539b5
    3dc5716b07 ramips: add NAS goodies on U25AWF-H1 + U35[N|W]F
    94bf2e4813 ipq40xx: drop unreachable ipq-wifi package for the AP120C-AC
    5ef3fe614c openssl: refresh patches
    e1dcfe02b2 mac80211: refresh patches
    0e22e14b6c mt76: update to the latest version
    c3a78955f3 kernel: move crypto-arc4 into a module
    bd926fdde5 ath10k-firmware: update Candela Tech firmware images
    5f013de786 kernel: bump 4.14 to 4.14.139
    491eaa898b kernel: bump 4.19 to 4.19.67
    79f235abef mtd-utils: update to 2.1.1
    08e274da9d tools/mtd-utils: update to 2.1.1
    e10b163247 ar71xx: ag71xx: add missing register writes
    662922068d ar71xx/ath79: ag71xx: get ring_mask consistent
    e18724b907 ar71xx: ag71xx: pass correct device pointer to dma
    4d40c0edd4 ar71xx: ag71xx: replace ag71xx_get_phy_if_mode_name with phy_modes()
    19ed799bdc ar71xx: ag71xx: make use of managed dev API simplifying code
    e773838813 ar71xx: ag71xx: use base address value directly from the source
    81d0da1186 ar71xx/ath79: ag71xx: dont fetch the same var again
    2340c646e6 brcm2708: update to latest patches from RPi foundation
    2876709fb7 brcm2708: fix DMA leaks in bcm2835-mmc
    cb3c4c713d brcm2708-gpu-fw: update to latest firmware
    b13f3300d5 iwinfo: update to latest Git HEAD
    517cb0b70b fstools: update to latest Git HEAD
    b32c77004c kernel: bump 4.9 to 4.9.190
    b5407887bc kernel: bump 4.14 to 4.14.140
    1bc31e0339 kernel: bump 4.19 to 4.19.68
    5cc942a80e ath9k: backport dynack improvements
    4685fc0cf1 mediatek: update v4.19 switch support to latest vendor version
    6e45ba4699 procd: fix compile issue with glibc (FS#2469)
    775b70f8d5 scripts/feeds: allow adding parameters to feeds
    7d542dc804 kernel: add disable_eap_hack sysfs attribute
    055abe902b brcm2708: remove duplicated patch
    bec2df9a66 mvebu: sysupgrade: fix uDPU eMMC check regression
    a58bfb7377 mac80211: rt2x00: revert commit causing regression in 5GHz band

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=c262daf308e0f0bd93bb5c5ee6238773935079ee
+OPENWRT_COMMIT=a58bfb7377de2ccda55d66c78b0378d5d6ec7af9
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

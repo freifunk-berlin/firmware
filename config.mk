30b407c57365338d6739ad6f59e8997f8b6a386f
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-08-31 00:30:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    c85f9a245a odhcpd: remove socket from uloop upon NETEV_IFINDEX_CHANGE
    d720f83f87 mdadm: Fix compile with musl 1.1.23
    7d300326ee mdadm: Use upstream fix for musl 1.1.23 compile
    8bf8de95a2 imagebuilder: fix `make info` for empty SUPPORTED_DEVICES
    1a1a504a54 mac80211: add new minstrel_ht patches to improve probing on mt76x2
    7c32f8eff0 mac80211: backport support for the IEEE80211_KEY_FLAG_GENERATE_MMIE flag
    c3327f48dc mt76: update to the latest version
    f830903c37 ath10k-ct: Update to 2019-06-13
    3898d98061 ath10k-firmware: update Candela Tech firmware images
    e57a756487 ath10k-firmware: update board-2.bin for community firmwares
    3a2bdf91bf ath10k-firmware: Fix mirror hash
    10671619dc ath79: Fix whitespace errors in 11-ath10k-caldata
    218cc22a43 ath10k-ct: update to version 2019-06-13
    5c47bfb735 ath10k-ct: remove patches for old versions
    c46219021f ath10k-firmware: update Candela Tech firmware images
    9952c8cf40 ath10k-ct: update to version 2019-08-14
    8fba3ebe88 kernel: bump 4.14 to 4.14.139
    c03765e87a mtd-utils: update to 2.1.1
    faaa52eff9 tools/mtd-utils: update to 2.1.1
    576791b9b4 ar71xx: ag71xx: add missing register writes
    42b42d2c9b ar71xx/ath79: ag71xx: get ring_mask consistent
    d143a0a4f6 ar71xx: ag71xx: pass correct device pointer to dma functions
    c9b28b5703 ar71xx: ag71xx: replace ag71xx_get_phy_if_mode_name with phy_modes()
    8d78aa8eaf ar71xx: ag71xx: make use of managed dev API simplifying code
    db1a935e5c ar71xx: ag71xx: use base address value directly from the source
    0a59e91973 ar71xx/ath79: ag71xx: dont fetch the same var again
    6710bb25b6 iwinfo: update to latest Git HEAD
    f2c6fa3dbb fstools: update to HEAD of 2019-07-01 - 1539b5
    ee164007b0 fstools: update to latest Git HEAD
    9e837873e7 kernel: bump 4.14 to 4.14.140
    dd4b8a24a1 ath9k: backport dynack improvements
    73d45c7378 mac80211: add config tweak for tx bursting when using VHT
    22378beb58 iproute2: add libcap support, enabled in ip-full
    db94ee6256 kernel: bump 4.14 to 4.14.141

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b2cb6bb48abbc0521fbd1a928c37f4846427a959
+OPENWRT_COMMIT=db94ee62567c786bc7ca2c1ed0bcf650b77a598e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

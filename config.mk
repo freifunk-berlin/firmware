7d0812706f580fa4a743a7859c83358d6b3e3c1d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-09-02 00:30:11 +0200

    Update to HEAD of OpenWrt-19.07 branch
    4d5a47d206 gemini: Stop enforcing command line
    a5762b45c2 ath79: etactica-eg200: fix restore button
    4fb6b8c553 ar71xx: WNR2200: remove redundant GPIO for WLAN LED

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=db94ee62567c786bc7ca2c1ed0bcf650b77a598e
+OPENWRT_COMMIT=4fb6b8c553f692eeb5bcb203e0f8ee8df099e77e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

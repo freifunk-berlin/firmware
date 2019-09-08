16a1f908ae7e69ccff6babd3e80ad984619c2030
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-09-09 00:30:15 +0200

    Update to HEAD of OpenWrt-19.07 branch
    b8b62b8506 ramips: disable badblock shifting for MT7621 NAND
    f057133349 ramips: use phy trigger for various Netgear boards
    15d84131fe ramips: fix network setup for various NETGEAR boards
    db7a1e7c04 ath79: correct OCEDO Ursus phy-mode property
    986c706cd1 odhcp6c: update to latest git HEAD
    5ae87c76b7 treewide: sysupgrade: don't use $ARGV in platform_do_upgrade()
    15292501a1 mvebu: sysupgrade: don't use $ARGV in platform_check_image()
    16985d2aab kernel: net_sched: fix a NULL pointer deref in ipt action
    271990321a ipq40xx: fix AVM NAND caldata extraction
    4c8258c925 Revert "build: remove harmful -nopad option from mksquashfs"
    ccd1335dcc apm821xx: Make patches apply again
    6d2044d62f mt7620: disable image generation for Nexx WT3020 (4MB)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=afce041e2bbe44126a5908ec9bf1d18f5177603b
+OPENWRT_COMMIT=6d2044d62f85b190d6279bf634efd00d55b28669
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

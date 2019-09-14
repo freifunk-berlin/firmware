0fc80edfe486f7e651ad1941b1a22514a8e972bd
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-09-15 00:30:14 +0200

    Update to HEAD of OpenWrt-19.07 branch
    c7f710e474 mac80211: brcmfmac: backport more kernel 5.4 changes
    f9ecee7913 ramips: remove duplicate case for MAC setup of freestation5
    79b9bc44d6 ramips: fix duplicate network setup for dlink, dir-615-h1
    d889cc9887 ramips: fix ethernet MAC address of ASUS RT-AC57U
    a0c8494704 hostapd: Fix security problem in EAP-pwd
    e8c5e6177d hostapd: SAE/EAP-pwd side-channel attack update
    2cd89cf0d7 kernel: bump 4.14 to 4.4.142
    af91a370de ar71xx: use platform code for qca955x usb0 init
    a3c3a8c5ab ar71xx: enable ddr wb flush on qca955x
    9ba9a91343 ar71xx: qca955x pci init/reset fixes
    3b8239e93e Revert "ar71xx: use platform code for qca955x usb0 init"
    f54bc3985a tools: mkimage: fix __u64 typedef conflict with new glibc
    37caec2d5e treewide: don't hardcode "sysupgrade.tgz" file name
    1b9a4f0cb4 treewide: when copying a backup file always specify dest name
    a717428828 treewide: use new procd sysupgrade $UPGRADE_BACKUP variable
    87fe1a560a brcm47xx: extend firmware validation
    3dac34de25 bcm53xx: extend firmware validation
    78d0d13c86 base-files: validate firmware for compatibility with backup
    63c0a08d49 kernel: bump 4.14 to 4.14.143
    ca3c7a8868 ar71xx: Fix potentially missed IRQ handling during dispatch
    febf9de9cf ar71xx: fix potential IRQ misses during dispatch for qca953x
    cf3b50377e ar71xx: make IRQ fixes target specific

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6d2044d62f85b190d6279bf634efd00d55b28669
+OPENWRT_COMMIT=cf3b50377e9be1208489c5330776e48e318e38ec
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

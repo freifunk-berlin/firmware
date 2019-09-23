0784b6b6172e8f95cf0cac8ac74221a1029e030d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-09-24 00:30:19 +0200

    Update to HEAD of OpenWrt-19.07 branch
    e1cf17b3ba iwinfo: update to latest Git HEAD
    0e0f9ff93c ath79: fix UniFi AC LED mapping
    169bb3d0cb scripts/feeds: fix accepting "-" in feed type string
    0da990b773 odhcpd: retry failed PD assignments on addrlist change
    47a5f5c7e7 base-files: sysupgrade: pass "backup" ubus attribute
    f69b855a75 procd: update to the latest git HEAD
    c53a0ed5e3 treewide: sysupgrade: use $UPGRADE_BACKUP to check for backup
    efa2db42b2 ar71xx: fix typo in platform_do_upgrade_compex()
    bece406c2f mac80211: brcmfmac: backport the last 5.4 changes
    f15441384c bcm53xx: sysupgrade: support Luxul firmware format
    11644ec8e1 brcm47xx: sysupgrade: support Luxul firmware format
    250d57ac29 ramips: add factory image for NETGEAR R6220
    ad8b11213a procd: update to the latest git HEAD
    c7e3ca59ab firewall: update to latest Git HEAD
    9cae5a8289 procd: fix invalid JSON filter expression in procd_running()
    d14aa19904 kernel: bump 4.14 to 4.14.144
    cd96cdaa60 kernel: bump 4.14 to 4.14.145
    e9c16e4e1f kernel: add module for Emulex OneConnect 10Gbit
    44f32cd5e0 ltq-vdsl-fw: update firmware filename and download URL
    e105d03ee9 mbedtls: update to 2.16.3
    dc076160f9 uClibc++: Remove faulty patch
    26c0bec13b hostapd: Fix AP mode PMF disconnection protection bypass
    0b9f3c28ef ath79: remove invalid uses of ath9k_patch_fw_mac_crc
    b610572a9b openssl: bump to 1.1.1d
    d897b32139 kernel: add disable_eap_hack sysfs attribute

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=cf3b50377e9be1208489c5330776e48e318e38ec
+OPENWRT_COMMIT=d897b32139a3c2f80b7a7f688dc274327a313aad
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

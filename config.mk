a23173d044cbfc5854604b83b846097861edb078
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-09-26 00:09:59 +0200

    remove ffberlin-builder specific files

--- config.mk
+++
@@ -1,8 +0,0 @@
-# default parameters for Makefile
-SHELL:=$(shell which bash)
-TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
-OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d897b32139a3c2f80b7a7f688dc274327a313aad
-SET_BUILDBOT=env
-MAKE_ARGS=V=s
--- packages/backbone_4MB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This is a custom postinst script that gets run by a (patched)
-# ImageBuilder Makefile in the target root after installing OpenWrt
-# packages, just before building the SquashFS image.
-
-echo "Deleting OLSR i18n files..."
-rm -vf usr/lib/lua/luci/i18n/olsr.*

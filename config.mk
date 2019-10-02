5a5449c5e15ac9defec070bb4a21dd1e6c835e7b
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-03 00:30:15 +0200

    Update to HEAD of OpenWrt-19.07 branch
    8a26f2a0a1 kernel: bump 4.14 to 4.14.146
    757fd85402 ath10k-firmware: update Candela Tech firmware images
    92953ae99f ath10k-ct: update to version 2019-09-09
    a48ed75d6c ipq40xx: abort ar40xx probe on missing PHYs
    c7cdbf29c0 uboot-fritz4040: update to 2019-09-07
    9200b4a6a0 brcm47xx: sysupgrade: fix device model detection
    ac04be82c4 netfilter: fix crash in flow offload by adding netns support
    151bd9ee25 kernel: port upstream nft_flow_offload changes to xt_FLOWOFFLOAD and fix routing issues
    7594c5e6dc apm821xx: fix fan control on highest step
    00e4d3e845 ipq40xx: fix hw-crypto detection of qce driver
    ad07166ea7 ipq806x: remove unsupported hw-crypto qce driver
    fb5a6d4926 ath79: Correct glinet, gl-ar300m-lite in 02_network
    5a0a969e41 ath79: Restore GL.iNet GL-AR300M-Lite first-boot connectivity
    b5a70a3fcd ath79: dts: fix ja76pf2 spi frequency
    6cdd518eb1 ar71xx: sysupgrade: accept ath79 combined-image
    4e93d17f2a ath79: image: append metadata to routerstations and ja76pf2 images
    39ae849d0d ath79: image: add supported string for routerstations and ja76pf2
    28d3afc8d6 base-files: use get_mac_binary() in mtd_get_mac_binary_ubi()

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d897b32139a3c2f80b7a7f688dc274327a313aad
+OPENWRT_COMMIT=28d3afc8d67231c2ad7adbde3e4b1179d0648c0e
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

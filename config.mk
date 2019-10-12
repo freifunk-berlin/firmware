bd9521c3bd3dd898d9cb142564d1cef2cf3400f5
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-13 00:10:13 +0200

    Update to HEAD of OpenWrt-master
    9a4fb78e7c iw: Update to version 5.3
    06091afada bcm53xx: add pending patch adding Luxul XWC-2000 DTS file
    64b7dbb300 bcm53xx: fix sysupgrade for Luxul XWC-2000
    fedc5b65dc bcm53xx: fix serial console on Luxul XWC-2000
    7449a39c0e kernel: bump 4.19 to 4.19.76
    1ed5c1b146 odhcpd: update to latest git HEAD
    27bf8abe69 firewall: update to latest git HEAD
    34e2526f9f kernel: add kmod-rtc-pcf2127
    97c37f8dd0 mac80211: ath10k: fix latency issue for QCA988x
    b4af2c689f expat: Update to version 2.2.9
    3b874c2c81 sunxi: update kernel config
    794d4b6652 treewide: remove kmod-usb-core from DEVICE_PACKAGES
    929becbc2d ath79: fix whitespace issues in DTS files
    67d019ac94 ramips: enable LED VCC for Asus RT-AC51U
    273a6cb562 ath79: improve DTS for TP-Link Archer D50 v1
    65fad9fec5 kernel: bump 4.9 to 4.9.195
    9b04a7576b kernel: bump 4.14 to 4.14.147
    8e197c7af2 kernel: bump 4.19 to 4.19.77
    f516ce3b10 kernel: bump 4.9 to 4.9.196
    4648533ca4 kernel: bump 4.14 to 4.14.148
    8184429b7b kernel: bump 4.19 to 4.19.78
    9c033242b1 mac80211: add an improved moving average algorithm to minstrel
    700e7a2eb9 tools/e2fsprogs: Update to version 1.45.4
    a92f74ba8d libnl-tiny: move source code into separate Git repository
    b204fcdb07 target/imagebuilder: use multi-thread support for xz compression
    a9e4e595e1 openssl: add gcc-8 -ffile-prefix-map filter
    bd4d3cd101 toolchain,build: prefer -ffile-prefix-map for gcc-8+
    419eff50f9 config: remove unused GCC_VERSION_4_8 config symbols
    d509463816 toolchain/gcc: switch to version 8 by default
    4ed356fa71 kernel.mk: add KCFLAGS to make kmods reproducible
    e0ce80d42a kernel: Fix off-by-one error in FIT mtd partition search.
    3a4f587c46 base-files: upgrade: add case to export_bootdevice
    f690b6f472 mac80211: fix scan when operating on DFS channels in ETSI domains
    46a129194d build: adjust gcc/g++ version checks for newer apple compilers
    f8b58757d0 ppp: update to version 2.4.7.git-2019-10-04
    7a8bcf36c7 mt76: update to the latest version
    33dd522f24 build: fix menuconfig submenu sorting for gcc options
    36c6f4a011 libnl-tiny: fix package mirror hash
    f3c21d1517 omap: split up DEVICE_TITLE
    c3ffb0e7d5 brcm47xx: split up DEVICE_TITLE
    9caa86fba5 kernel: kmod-rtc-pcf2127: Fix dependencies
    e901ce6f0e kernel: Set GCC version to 80300 by default
    ace1bfb554 mvebu: Refresh kernel 4.19 configuration
    44f11353de libpcap: update to 1.9.1
    394273c066 tcpdump: update to 4.9.3
    68014f6d6e mediatek: remove unnecessary execute permission bit
    b83b2d76ca ar71xx: remove unnecessary execute permission bit
    60acddc960 uboot-oxnas: remove unnecessary execute permission bit
    9f73fad359 kernel: fix typos in video KernelPackage description
    66fbaae83e pistachio: split up DEVICE_TITLE
    e11fc8439c kirkwood: Fix FS#505 - Can't install LEDE on Linksys EA3500
    d2a1075973 mvebu: backport mvneta and comphy from linux 5.x
    c5829788c9 lantiq: add set_port_link feature to xrx200-net driver
    6ab386c9bc uClibc++: Fix three bugs
    ee5a3f6d60 hostapd: adjust to removal of WOLFSSL_HAS_AES_GCM
    b10a453367 base-files: coreutil-date breaks setting kernel timezone
    b30e481b6c kernel: NFSD: add dependency kmod-crypto-arc4 for kmod-fs-nfs-common-rpcsec
    77cfc0739d trelay: handle netdevice events correctly
    c2635b871d trelay: fix deadlock on remove
    10f5eb0398 trelay: log "started" and "stopped"

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=4791afa734d4754cd92c65feffc60e62e4dc5c60
+OPENWRT_COMMIT=10f5eb039854f1d81512f6a50b58756ea151c825
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

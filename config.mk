d2b88b74110331965fb480c7ce5bfc218e777977
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-16 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    4b9a07336a kernel: add kmod-iio-bme680
    ac36cca012 ramips: mt7621: Alternative name KE 2P for Phicomm K2P
    bba6646b5c ath79: add support for COMFAST CF-E313AC
    c1388a2deb base-files: move xor() from caldata extraction to functions.sh
    5b6a809092 treewide: move calibration data extraction function to library
    2c60de0e3f treewide: move MAC address patch functions to common library
    34c4741da0 odhcpd: update to latest git HEAD
    57b834281b iwinfo: update to latest Git HEAD
    4d5b62263c toolchain/musl: bump to version 1.1.24
    1cfe412dd2 kernel: bump 4.14 to 4.14.149
    e5445333f6 kernel: bump 4.19 to 4.19.79
    68c50e1ca2 toolchain/gdb: bump to 8.3.1
    089b4f16aa gdb: bump to 8.3.1
    07c1ddf522 ath79: add support for UniFi AC-LR
    07e555d873 mpc85xx: correct TP-LINK TL-WDR4900 MAC addresses
    1a256470e7 ath79: correct ar71xx boardname for UniFi AC Lite
    332b1f46e9 ipq40xx: add label MAC address for FritzBox 4040
    48b5d08a48 treewide: use a single ath10k MAC patching function with checksum
    4533ba6810 lua: fix linking under glibc
    e56e5a454e brcm47xx: fix switch port labels for Asus WL500GP V2
    63c43e5674 bcm53xx: sysupgrade: support Luxul NAND devices
    f351beedfd libevent2: Update to 2.1.11
    71e04091a9 lantiq: fix dsl_control status handling.
    a5ec41b0e5 mac80211: add new acs_exclude_dfs option
    d1072096f4 ramips: fix WiFi MAC addresses for D-Link DIR-810L

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=10f5eb039854f1d81512f6a50b58756ea151c825
+OPENWRT_COMMIT=d1072096f49823eb39357f9555d7854a9c91bcfb
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

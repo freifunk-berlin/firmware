76ec80fe864b6aea940a60cd70d13a2445acf8dc
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-19 00:10:16 +0200

    Update to HEAD of OpenWrt-master
    bc61458b73 iwinfo: update to latest Git HEAD
    14881b49d8 toolchain/kernel-headers: don't use TARGET_CFLAGS
    9d5e266cb1 wireguard: bump to latest snapshot 20191012
    6caf437652 build: add buildinfo as single Makefile target
    889b841048 fwtool: do not omit final 16 byte when image does not contain signature
    07b5c59232 kernel: fix MBIM description
    d544bc84a0 lantiq: Fix fw_cutter LzmaWrapper
    d3b0459c93 lua: install luac symlink on host
    9105057cc0 6in4: add rfc1918 check function
    91aabae895 util-linux: add more command
    ddf6ec29b4 procd: allow usage of * as procd_running() instance parameter
    c45c7606cc wwan: check new uci bus option on proto setup event
    68b1b0bb39 wwan: add mc7304 to database
    a78a539afd wwan: add ec25 to database
    c7c14aaad3 wwan: Double quote to prevent globbing and word splitting
    9bb5dac593 build: fix seq host tool check
    54d9ac8ba8 ipq806x: WPQ864: fix partition unit-address

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d1072096f49823eb39357f9555d7854a9c91bcfb
+OPENWRT_COMMIT=54d9ac8ba822e76a2667ce3b49c9be4496c5eba4
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

8d61d913f17e4eb7e5638f3057779883bd5d4c5d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-20 00:30:19 +0200

    Update to HEAD of OpenWrt-19.07 branch
    047ef4b39a bcm53xx: add pending patch adding Luxul XWC-2000 DTS file
    a66dd84758 bcm53xx: fix sysupgrade for Luxul XWC-2000
    b3d70f628b bcm53xx: fix serial console on Luxul XWC-2000
    ac1858ddf0 treewide: remove 4.19 leftovers
    fd82b4fe70 kernel: add missing symbol when enabling PTP support
    c4b514b200 kernel: bump 4.14 to 4.14.147
    60ff8bf512 kernel: bump 4.14 to 4.14.148
    830000855e build: make device tree arg really optional in mkits.sh
    4abf456b4a kernel: Correctly search for the FIT image in mtd partition.
    5e9aae9ef0 kernel: Fix off-by-one error in FIT mtd partition search.
    f2c321fc07 build: fix host menu config targets using ncurses
    53a6aa5164 build: fix xconfig target
    fcf87d5461 build: adjust gcc/g++ version checks for newer apple compilers
    e8dc69fcb0 iwinfo: update to latest Git HEAD
    49c563a829 ramips: mt7621: add support for memory detection
    2155e94d4b ramips: remove memory node for ZBT-WE1326
    4c92859945 iwinfo: update to latest Git HEAD
    00f96dcddb toolchain/musl: bump to version 1.1.24
    f3a265575c kernel: bump 4.14 to 4.14.149
    04c81498df toolchain/gdb: bump to 8.3.1
    e8308747a1 gdb: bump to 8.3.1
    6179bf5be1 brcm47xx: fix switch port labels for Asus WL500GP V2
    f4b1926be0 bcm53xx: sysupgrade: support Luxul NAND devices
    180bd75973 iwinfo: update to latest Git HEAD
    07dcbfa6e8 fwtool: do not omit final 16 byte when image does not contain signature
    5bd83825ad rpcd: update to latest Git HEAD
    d04c07003d rpcd: update to latest Git HEAD
    c987955f82 rpcd: update to latest Git HEAD
    3468a4435a lantiq: Fix fw_cutter LzmaWrapper
    6fe6853d7e build: add buildinfo as single Makefile target
    cabd12d2e3 mac80211: Update to version 4.19.79
    8c8212c680 tplink-safeloader: fix compilation warnings
    ddff739d15 kernel: layerscape: fix compilation error
    3b1d71ffd8 bzip2: add linker option LDFLAGS
    9784a470bb trelay: handle netdevice events correctly
    b17c95bbdc trelay: fix deadlock on remove
    82a3beac9d hostapd: adjust to removal of WOLFSSL_HAS_AES_GCM
    5ad47b1ed8 uClibc++: Fix three bugs
    69586db84b kirkwood: Fix FS#505 - Can't install LEDE on Linksys EA3500
    0f3c06b0a5 kernel: fix typos in video KernelPackage description
    8e78bbb7bb libpcap: update to 1.9.1
    e5ab602c69 tcpdump: update to 4.9.3
    bcb0fd1fa1 mpc85xx: correct TP-LINK TL-WDR4900 MAC addresses
    b058da3a7b ath79: correct ar71xx boardname for UniFi AC Lite

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=28d3afc8d67231c2ad7adbde3e4b1179d0648c0e
+OPENWRT_COMMIT=b058da3a7b647100c5e2e8868692322473fd0055
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

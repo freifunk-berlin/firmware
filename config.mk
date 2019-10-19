866fe04167d0b76b215ec16b8e07b09fd12dc783
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-20 00:10:15 +0200

    Update to HEAD of OpenWrt-master
    67dc023f87 mac80211: Update to version 5.3.6
    e6cadb215c mac80211: Update to version 5.4-rc2
    97bcbc690c kernel: add package for Epson RX-8025 and compatible I2C RTC
    56ab58fb6c bzip2: add linker option LDFLAGS
    53b73131e9 kernel: layerscape: fix compilation error
    5daf094353 tplink-safeloader: fix compilation warnings
    1997feb2ff rb532: split up DEVICE_TITLE
    e6114ff925 at91: split up remaining DEVICE_TITLE definitions
    036f1d9bac octeon: split up remaining DEVICE_TITLE
    0aca8dc6ad mediatek: split up DEVICE_TITLE
    88f8b63a3d mvebu: split up DEVICE_TITLE
    34939711a5 ath25: split up DEVICE_TITLE

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-openvpn tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=54d9ac8ba822e76a2667ce3b49c9be4496c5eba4
+OPENWRT_COMMIT=34939711a51733d6461a0a653776c6de0804fa48
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

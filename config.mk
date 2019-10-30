506cc0d7b7e2f4551c7cab07f231d0932c384cf0
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-10-31 00:30:17 +0100

    Update to HEAD of OpenWrt-19.07 branch
    98da3c8cdb ath10k-firmware: retrieve wave 1 firmware from kvalo
    0e7113e6ec ath10k-firmware: update wave 1 firmware to 10.2.4-1.0-00047
    b7ee43ecac ar71xx: add model detection for UniFi AC-LR
    8cf67728d3 ath79: add support for UniFi AC-LR
    c51a39d4a5 mac80211: add an improved moving average algorithm to minstrel
    79e6391600 kernel: bump 4.14 to 4.14.150
    2436e521b4 kernel: mark kmod-usb-serial-wwan as hidden

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=8385d121d0ddfade4fa4755c1465110d9accfabb
+OPENWRT_COMMIT=2436e521b4ccc1c1fde5527604ef11bfcbbe5953
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

54b8c98846501ac39147adceb773500afa9f6880
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2019-11-04 00:30:14 +0100

    Update to HEAD of OpenWrt-19.07 branch
    7f29dc0fda kernel: bump 4.14 to 4.14.151
    466d499d03 rpcd: update to latest Git HEAD
    f051a967b8 libevent2: Update to 2.1.11
    8f0deedc11 ramips: fix WiFi MAC addresses for D-Link DIR-810L
    e539dc2daa ath79: Clean up GL-AR300M DTS/DTSI inclusions
    9b2a147cfe ramips: enable external amplifier for D-Link DIR-810L
    7407e72399 ipqx0xx: add Generic subtarget
    22c443c20c uboot-fritz4040: build with ipq40xx "generic" subtarget

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2436e521b4ccc1c1fde5527604ef11bfcbbe5953
+OPENWRT_COMMIT=22c443c20cab4760b201ccd9995a4ed6053aa810
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

5e47c9796a8946657e523aef910a22ef3bb622cc
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-11-12 21:55:00 +0100

    import basic scripts from gloun for patching OpenWrt

---
+++ scripts/feeds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+. scripts/modules.sh
+. scripts/default_feeds.sh
+
+
+rm -rf openwrt/tmp
+rm -rf openwrt/feeds
+rm -rf openwrt/package/feeds
+
+(
+	echo 'src-link gluon_base ../../package'
+	for feed in $FEEDS; do
+		echo "src-link $feed ../../packages/$feed"
+	done
+	for feed in $(echo "$DEFAULT_FEEDS" | grep -vxF "$FEEDS"); do
+		echo "src-dummy $feed"
+	done
+) > openwrt/feeds.conf
+
+openwrt/scripts/feeds update -a
+openwrt/scripts/feeds install -a
---
+++ scripts/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+. scripts/modules.sh
+
+
+GLUONDIR="$(pwd)"
+
+for module in $GLUON_MODULES; do
+	echo "--- Updating module '$module' ---"
+	var=$(echo "$module" | tr '[:lower:]/' '[:upper:]_')
+	eval repo=\${${var}_REPO}
+	eval branch=\${${var}_BRANCH}
+	eval commit=\${${var}_COMMIT}
+
+	mkdir -p "$GLUONDIR/$module"
+	cd "$GLUONDIR/$module"
+	git init
+
+	if ! git branch -f base "$commit" 2>/dev/null; then
+		git fetch "$repo" "$branch"
+		git branch -f base "$commit" || {
+		  echo "unable to find commit \"$commit\" on branch \"$branch\" in repo \"$repo\"." >&2
+		  exit 1
+		}
+	fi
+done

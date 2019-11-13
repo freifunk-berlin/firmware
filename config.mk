258613a56a0ffc81485fbde0ae12b307a9d9fb92
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-11-13 23:58:14 +0100

    switch to OpenWrt 19.07.0-rc1

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=1867f108075a875c3334d827beff74bc6c24f2f6
+OPENWRT_COMMIT=88bff692a56f29c9af402a633e02eb537d983874
 SET_BUILDBOT=env
 MAKE_ARGS=

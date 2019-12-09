72667f2f65164c542e8cddf2feca04bfbba958b7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-12-09 21:57:47 +0100

    Merge #713 rename image-flavors default and backbone
    In https://github.com/freifunk-berlin/firmware/issues/534 there was some discussion about meaningful names.
    * default --> notunnel
    * backbone --> manual

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin-tunneldigger backbone
+PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=d3e11e8ad85b9a51027174d5207e1e43a8c8fe94
 SET_BUILDBOT=env

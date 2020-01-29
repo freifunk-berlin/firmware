ce8d04dc3782fc9c17fc27469d3c6fdbb7261b77
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-01-30 00:10:14 +0100

    Update to HEAD of OpenWrt-19.07 branch
    4668ae3bed OpenWrt v19.07.1: revert to branch defaults
    a229907150 ramips: remove duplicate DEVICE_PACKAGES for TP-Link Archer C20i

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=901bbe2ab97efd3af0eb00efc3b413617145d654
+OPENWRT_COMMIT=a229907150e01d8c32293cc0a48630e7c4f0cb00
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

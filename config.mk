4ff3421391441e42789ac58e0ec5c597023ef1d3
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-01-31 00:10:13 +0100

    Update to HEAD of OpenWrt-19.07 branch
    aed6632d31 ramips: use tpt DTS trigger for TP-Link TL-MR3020 v3 and TL-WA801ND v5

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=a229907150e01d8c32293cc0a48630e7c4f0cb00
+OPENWRT_COMMIT=aed6632d31ff5d29045dc904dedc840d902aad97
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

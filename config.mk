3c3b4bac8b5d3989babca289a6e0fa6900165e13
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-05 00:10:15 +0100

    Update to HEAD of OpenWrt-19.07 branch
    eca8a2ee0d kernel: bump 4.14 to 4.14.169
    2a844349fa kernel: add support for GD25D05 SPI NOR

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=3d1c84d424c4f19f6e5c4c63418f033b26ec35ff
+OPENWRT_COMMIT=2a844349fa3cd6b9ffcc82dca00fb72b4c110cee
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

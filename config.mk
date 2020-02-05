8b40af4c976424ec93b93e3b1ace4f5e6feb338d
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-06 00:10:15 +0100

    Update to HEAD of OpenWrt-19.07 branch
    d91b52b1a2 kernel: add missing symbol

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=2a844349fa3cd6b9ffcc82dca00fb72b4c110cee
+OPENWRT_COMMIT=d91b52b1a2edd3645c88b29deb1091ae5f82fda8
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

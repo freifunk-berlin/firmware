93ec193b56442d922f3e5a413305e50ce2f93a82
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-08 00:10:18 +0100

    Update to HEAD of OpenWrt-19.07 branch
    887eb669f9 mac80211: brcm: backport remaining 5.6 kernel patches
    cf2b042855 firmware-utils: add lxlfw tool for generating Luxul firmwares
    cff3795450 bcm53xx: build images for Luxul ABR-4500 and XBR-4500 routers
    53cd2299ee ath79: WNDR3700 v1/v2: make u-boot env partition writable
    a0ca72d9ab uboot-envtools: ath79: add Netgear WNDR3700v2
    5000fc53a1 ath79: fix DTS node names for Ubiquiti XW partitions

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d91b52b1a2edd3645c88b29deb1091ae5f82fda8
+OPENWRT_COMMIT=5000fc53a170ae6611b649767740238635be6f30
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

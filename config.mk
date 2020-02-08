da196c4528adfd573790c356d3d73f30524f8b1f
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-09 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    b6c01fec92 hostapd: remove erroneous $(space) redefinition

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5000fc53a170ae6611b649767740238635be6f30
+OPENWRT_COMMIT=b6c01fec921bb094416e70abad4c706b6cb030fd
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

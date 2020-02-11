b9d9b6537fd9393876eb1a17940ba4c7ec3ccdc7
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-12 00:10:11 +0100

    Update to HEAD of OpenWrt-19.07 branch
    2d21357b65 ath79: ar93xx/qca95xx: move gmac/wmac/pcie node out of apb bus
    95d5cbdec3 ath79: add wmac migration for all ar93xx/qca95xx SoCs

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b6c01fec921bb094416e70abad4c706b6cb030fd
+OPENWRT_COMMIT=95d5cbdec34e3d29db17a2c823e3d01be1e9c283
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

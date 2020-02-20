7059780c7796d39ee7ab1b882d30211d385e7171
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-21 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    6b7eeb74db ppp: backport security fixes

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=95d5cbdec34e3d29db17a2c823e3d01be1e9c283
+OPENWRT_COMMIT=6b7eeb74dbf8b491b6426820bfa230fca60047dc
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

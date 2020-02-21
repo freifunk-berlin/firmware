9d6c6f62c411d786634f362b38b2fb3751f0070c
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-22 00:10:11 +0100

    Update to HEAD of OpenWrt-19.07 branch
    085f38351f ath79: enable forceless sysupgrade from ar71xx on fritz300e
    7cbd39421e ath79: add gpio4 pinmux on TL-WR841N/ND v8, WR842N v2, MR3420 v2
    6a950afde1 ath79: add support for Ubiquiti Nanostation Loco M (XM)
    8fa6107aee ath79: add support for Ubiquiti Picostation M (XM)
    2d3a93335a ramips: append tail to WF2881 initramfs image

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=6b7eeb74dbf8b491b6426820bfa230fca60047dc
+OPENWRT_COMMIT=2d3a93335ae4ea9ca63e4bae798738b5befca440
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

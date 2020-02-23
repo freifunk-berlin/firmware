582e0478037df7bb0ff14190481c594a71398fcc
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-24 00:10:16 +0100

    Update to HEAD of OpenWrt-19.07 branch
    c9b6bb43ce ath79: phy-ar7200-usb: adapt old behavior of arch/mips/ath79/dev-usb.c
    d0c8875faf ath79: ar934x: use reset for usb-phy-analog
    191822b59f ipq806x: add missing core1 voltage tolerance
    bc0ca20ca9 ipq806x: fix bug in L2 cache scaling

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b2660e67f0fb9f3398b871ad1fef5725e1b5d7d2
+OPENWRT_COMMIT=bc0ca20ca90e4d58090392c96b6d8d6405444268
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

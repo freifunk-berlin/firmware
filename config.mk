6dfb8c6c4fd7c18d95868606e7bd8cf1e6cebd2c
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-02-28 00:10:10 +0100

    Update to HEAD of OpenWrt-19.07 branch
    65030d81f3 libubox: update to latest Git HEAD
    33732f4a9c OpenWrt v19.07.2: adjust config defaults
    c56ed72d2b OpenWrt v19.07.2: revert to branch defaults

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b33cfb7eb242d3e839947486a0b77c34ad88a1cf
+OPENWRT_COMMIT=c56ed72d2bc6dbee3ff82b4bd42e1768f1a2c737
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

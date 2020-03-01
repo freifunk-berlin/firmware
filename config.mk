77933a32888d450670a514d9afd51148fc89575e
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-02 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    ef391799e3 ar71xx: correct AVM FRITZ Repeater 450E WPS button flag
    1713707673 ar71xx: add missing LED migration for Archer C7

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=c56ed72d2bc6dbee3ff82b4bd42e1768f1a2c737
+OPENWRT_COMMIT=17137076732b18442202e75c7edf10bccbc5f2a2
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

b8df353aa20ea39f06ba742a4c87982a62cc9402
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-05 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    f6f0cd54a2 rpcd: update to latest Git HEAD
    e7f1313bbb rpcd: add respawn param

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=17137076732b18442202e75c7edf10bccbc5f2a2
+OPENWRT_COMMIT=e7f1313bbbf44e529e5de40045b191e026af6b7b
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

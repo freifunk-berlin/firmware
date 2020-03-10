991a253f86a88dc8123058c73388eb9f43d7a09a
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-11 00:10:13 +0100

    Update to HEAD of OpenWrt-19.07 branch
    06f5a8d3e9 kernel: bump 4.14 to 4.14.172

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=e7f1313bbbf44e529e5de40045b191e026af6b7b
+OPENWRT_COMMIT=06f5a8d3e9667ff0bfee66df5f37f40991dbe326
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

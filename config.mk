fbd6b6fe4d2d6bd95162baf9e227c91c0a407d34
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-12 00:10:11 +0100

    Update to HEAD of OpenWrt-19.07 branch
    6e4453aecc kernel: backport out-of-memory fix for non-Ethernet devices
    68351990dc ar71xx/ath79: ew-dorin, fix the trigger level for WPS button
    9da31d0fb4 mt76: update to the latest version
    b32129d30b rssileds: add dependencies based on LDFLAGS

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=06f5a8d3e9667ff0bfee66df5f37f40991dbe326
+OPENWRT_COMMIT=b32129d30ba20c871a301c8627221b2467e60bf0
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

c97142bfda560fa5c3086dc7de38a0524a2af1db
Freifunk-Berlin Autoupdater <freifunk@it-solutions.geroedel.de>
2020-03-15 00:10:12 +0100

    Update to HEAD of OpenWrt-19.07 branch
    9f024d3587 ath79: fix swapped LAN/WAN MAC address for Archer C60 v1/v2
    83f1015a6c ar71xx: fix swapped LAN/WAN MAC address for Archer C60 v1/v2
    e4107e30a7 ar71xx: remove wrong MAC address adjustment for Archer C60 v2
    f1a3a6b79c ath79: fix port order on TP-Link Archer C60 v1/v2
    76c1c1daea ar71xx: fix port order on TP-Link Archer C60 v1/v2
    7ae345ecb7 ath79: add support for TP-Link TL-WR740N v5
    74a8e36975 layerscape: add kmod-i2c-mux to DEVICE_PACKAGES for traverse-ls1043

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=b32129d30ba20c871a301c8627221b2467e60bf0
+OPENWRT_COMMIT=74a8e36975a7148b4a901b849facbb6fa309ea97
 SET_BUILDBOT=env
 MAKE_ARGS=V=s

3c955484a6b83c45da90e7648c1d099722325f4b
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-05-10 18:00:00 +0200

    scripts/create_version-txt: handle "master" branches and default-feeds
    When using a master branch (no *_BRANCH set in modules file) there is no
    value printed in the branch-field. This adds a test, which sets "master"
    in such cases.
    Also don't print feeds to the VERSION.txt, which are just automatically
    created from teh feeds-default.conf.

--- scripts/create_version-txt.sh
+++ scripts/create_version-txt.sh
@@ -14,6 +14,7 @@ echo "https://wiki.freifunk.net/Berlin:Firmware" >> ${VERSION_FILE}
 echo "Firmware: git branch \"${GIT_BRANCH_ESC}\", revision $(${REVISION_CMD})" >> ${VERSION_FILE}
 # add openwrt revision with data from config.mk
 OPENWRT_REVISION=$(cd ${OPENWRT_DIR}; eval $REVISION_CMD base)
+[ -z ${OPENWRT_BRANCH} ] && OPENWRT_BRANCH=master
 echo "OpenWRT: repository from ${OPENWRT_REPO}, git branch \"${OPENWRT_BRANCH}\", revision ${OPENWRT_REVISION}" >> ${VERSION_FILE}
 # add feed revisions
 for FEED in `cd ${OPENWRT_DIR}; ./scripts/feeds list -n`; do \
@@ -22,5 +23,7 @@ for FEED in `cd ${OPENWRT_DIR}; ./scripts/feeds list -n`; do \
   FEED_GIT_REPO=$(echo PACKAGES_${FEED_UCASE}_REPO)
   FEED_GIT_BRANCH=$(echo PACKAGES_${FEED_UCASE}_BRANCH)
   FEED_REVISION=$(cd ${FEED_DIR}; ${REVISION_CMD} base)
-  echo "Feed $FEED: repository from ${!FEED_GIT_REPO}, git branch \"${!FEED_GIT_BRANCH}\", revision ${FEED_REVISION}" >> ${VERSION_FILE}
+  [ -z ${!FEED_GIT_BRANCH} ] && export ${FEED_GIT_BRANCH}=master
+  [ -z ${!FEED_GIT_REPO} ] || echo >> ${VERSION_FILE} \
+    "Feed $FEED: repository from ${!FEED_GIT_REPO}, git branch \"${!FEED_GIT_BRANCH}\", revision ${FEED_REVISION}"
 done

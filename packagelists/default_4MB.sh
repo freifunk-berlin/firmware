122d94f21541b9b7883ff5df3c4eab754ede192d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-06-06 01:30:09 +0200

    cleanup

--- packagelists/default_4MB.sh
+++ packagelists/default_4MB.sh
@@ -3,6 +3,3 @@
 # This is a custom postinst script that gets run by a (patched)
 # ImageBuilder Makefile in the target root after installing OpenWrt
 # packages, just before building the SquashFS image.
-
-echo "Deleting OLSR i18n files..."
-rm -vf usr/lib/lua/luci/i18n/olsr.*

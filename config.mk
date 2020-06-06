a096e5c9e116a6aaf9f3b77aa79d19a5343a87ce
Sven Roederer <devel-sven@geroedel.de>
2020-06-06 03:03:52 +0200

    config.mk: only build "default" profile

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
+PACKAGES_LIST_DEFAULT=default
 SET_BUILDBOT=env
 MAKE_ARGS=
